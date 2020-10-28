dc452a3abfdcec0b39e300ffcb2488779646c9b7
Leandro Lopes <leandrolopes@DESKTOP-9QDA1FV.localdomain>
2020-10-28 20:46:44 -0300

    Hiding label for mobile in the component Logo

--- src/components/Logo/styles.ts
+++ src/components/Logo/styles.ts
@@ -1,4 +1,5 @@
 import styled, { css } from 'styled-components'
+import media from 'styled-media-query'
 
 import { LogoProps } from '.'
 
@@ -11,13 +12,41 @@ const wrapperModifiers = {
   large: () => css`
     width: 20rem;
     height: 5.9rem;
+  `,
+
+  hideOnMobile: () => css`
+    /* 
+      Vai aplicar quando o valor for menor que o valor medium
+         huge: '1440px',
+         large: '1170px',
+      => medium: '768px',
+         small: '450px', 
+    */
+
+    ${media.lessThan('medium')`
+      width: 5.8rem;
+      height: 4.5rem;
+
+      svg {
+        height: 4.5rem;
+        /* Mesmo que o elemento esteja ocupando aquele espaço, 
+        faz os outros elementos não serem afetados 
+        */
+        pointer-events: none;
+      }
+
+      .text {
+        display: none;
+      }
+    `}
   `
 }
 
 export const Wrapper = styled.div<LogoProps>`
-  ${({ theme, color, size }) => css`
+  ${({ theme, color, size, hideOnMobile }) => css`
     color: ${theme.colors[color!]};
 
     ${!!size && wrapperModifiers[size]}
+    ${!!hideOnMobile && wrapperModifiers.hideOnMobile}
   `};
 `
