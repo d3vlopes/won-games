bbc29ad3ff57d4139a06a5a406533914dd786107
Leandro Lopes <leandrolopes@DESKTOP-9QDA1FV.localdomain>
2020-10-30 16:28:41 -0300

    Add prop icon on index Button component

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -4,14 +4,19 @@ export type ButtonProps = {
   children?: React.ReactNode
   size?: 'small' | 'medium' | 'large'
   fullWidth?: boolean
+  icon?: JSX.Element
 }
 
 const Button = ({
   children,
+  icon,
   size = 'medium',
   fullWidth = false
 }: ButtonProps) => (
-  <S.Wrapper size={size} fullWidth={fullWidth}>
+  // Retorna true ou false
+  <S.Wrapper size={size} fullWidth={fullWidth} hasIcon={!!icon}>
+    {icon}
+    {/* Se for children faz isso */}
     {!!children && <span>{children}</span>}
   </S.Wrapper>
 )
