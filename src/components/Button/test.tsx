1a3d0ad9108ec925664810c7dbe5ca555152d8ac
Leandro Lopes <leandrolopes@DESKTOP-9QDA1FV.localdomain>
2020-10-30 16:46:00 -0300

    Testing withIcon on Button component

--- src/components/Button/test.tsx
+++ src/components/Button/test.tsx
@@ -1,17 +1,20 @@
 import { screen } from '@testing-library/react'
+import { AddShoppingCart } from '@styled-icons/material/AddShoppingCart'
 import { renderWithTheme } from 'utils/tests/helpers'
 
 import Button from '.'
 
 describe('<Button />', () => {
   it('should render the medium size by default', () => {
-    renderWithTheme(<Button>Button</Button>)
+    const { container } = renderWithTheme(<Button>Button</Button>)
 
     expect(screen.getByRole('button', { name: /Button/i })).toHaveStyle({
       height: '4rem',
       padding: '0.8rem 3.2rem',
       'font-size': '1.4rem'
     })
+
+    expect(container.firstChild).toMatchSnapshot()
   })
 
   it('should render the small size', () => {
@@ -39,5 +42,15 @@ describe('<Button />', () => {
     expect(screen.getByRole('button', { name: /Button/i })).toHaveStyle({
       width: '100%'
     })
+  })
+
+  it('should render an icon version', () => {
+    renderWithTheme(
+      <Button icon={<AddShoppingCart data-testid="icon" />}>Button</Button>
+    )
+
+    // Verifica se existe no documento
+    expect(screen.getByText(/Button/i)).toBeInTheDocument()
+    expect(screen.getByTestId('icon')).toBeInTheDocument()
   })
 })
