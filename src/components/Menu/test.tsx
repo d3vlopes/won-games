29e82dd4da3101471a9bdb427d6e9cb571bb6d06
Leandro Lopes <leandrolopes@DESKTOP-9QDA1FV.localdomain>
2020-11-02 14:58:18 -0300

    Testing acessibility on Menu for mobile

--- src/components/Menu/test.tsx
+++ src/components/Menu/test.tsx
@@ -1,13 +1,17 @@
-import { render, screen } from '@testing-library/react'
+import { screen } from '@testing-library/react'
+import { renderWithTheme } from 'utils/tests/helpers'
 
 import Menu from '.'
 
 describe('<Menu />', () => {
-  it('should render the heading', () => {
-    const { container } = render(<Menu />)
+  it('should render the menu', () => {
+    renderWithTheme(<Menu />)
 
-    expect(screen.getByRole('heading', { name: /Menu/i })).toBeInTheDocument()
+    // Testa os arial-labels
+    expect(screen.getByLabelText(/abrir menu/i)).toBeInTheDocument()
+    expect(screen.getByLabelText(/pesquisar/i)).toBeInTheDocument()
+    expect(screen.getByLabelText(/abrir carrinho/i)).toBeInTheDocument()
 
-    expect(container.firstChild).toMatchSnapshot()
+    expect(screen.getByRole('img', { name: /won games/i })).toBeInTheDocument()
   })
 })
