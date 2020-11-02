d19e5f2f2acf392f2cf9d4014dea196def993d58
Leandro Lopes <leandrolopes@DESKTOP-9QDA1FV.localdomain>
2020-11-02 15:29:53 -0300

    Testing open/close on Menu  component for mobile

--- src/components/Menu/test.tsx
+++ src/components/Menu/test.tsx
@@ -1,4 +1,4 @@
-import { screen } from '@testing-library/react'
+import { fireEvent, screen } from '@testing-library/react'
 import { renderWithTheme } from 'utils/tests/helpers'
 
 import Menu from '.'
@@ -13,5 +13,26 @@ describe('<Menu />', () => {
     expect(screen.getByLabelText(/abrir carrinho/i)).toBeInTheDocument()
 
     expect(screen.getByRole('img', { name: /won games/i })).toBeInTheDocument()
+  })
+
+  it('should handle the open/close mobile menu ', () => {
+    renderWithTheme(<Menu />)
+
+    // selecionar o MenuFull
+    const fullMenuElement = screen.getByRole('navigation', { hidden: true })
+
+    // verificar se o menu está escondido
+    expect(fullMenuElement.getAttribute('aria-hidden')).toBe('true')
+    expect(fullMenuElement).toHaveStyle({ opacity: 0 })
+
+    // clicar no botão de abrir o menu e verificar se ele abriu
+    fireEvent.click(screen.getByLabelText(/abrir menu/i))
+    expect(fullMenuElement.getAttribute('aria-hidden')).toBe('false')
+    expect(fullMenuElement).toHaveStyle({ opacity: 1 })
+
+    // clicar no botão de fechar o menu e verificar se ele fechou
+    fireEvent.click(screen.getByLabelText(/fechar menu/i))
+    expect(fullMenuElement.getAttribute('aria-hidden')).toBe('true')
+    expect(fullMenuElement).toHaveStyle({ opacity: 0 })
   })
 })
