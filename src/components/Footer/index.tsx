458274e5818314fa2fb219fd1b237678aa91c2e0
Leandro Lopes <leandrolopes@DESKTOP-9QDA1FV.localdomain>
2020-11-04 16:47:03 -0300

    Create structure for Footer component

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,8 +1,87 @@
+import Link from 'next/adsdsds'
+
+import Heading from 'components/Heading'
+import Logo from 'components/Logo'
 import * as S from './styles'
 
 const Footer = () => (
   <S.Wrapper>
-    <h1>Footer</h1>
+    <Logo color="black" />
+    <S.Content>
+      <S.Column>
+        <Heading color="black" size="small" lineBottom lineColor="secondary">
+          Contato
+        </Heading>
+
+        <a href="mailto:sac@wongames.com">sac@wongames.com</a>
+      </S.Column>
+
+      <S.Column>
+        <Heading color="black" lineColor="secondary" lineBottom size="small">
+          Siga-nos
+        </Heading>
+
+        <nav aria-labelledby="social media">
+          <a
+            href="https://www.instagram.com/won-games"
+            target="_blank"
+            rel="noopenner, noreferrer"
+          >
+            Instagram
+          </a>
+          <a
+            href="https://www.twitter.com/won-games"
+            target="_blank"
+            rel="noopenner, noreferrer"
+          >
+            Twitter
+          </a>
+          <a
+            href="https://www.youtube.com/won-games"
+            target="_blank"
+            rel="noopenner, noreferrer"
+          >
+            Youtube
+          </a>
+          <a
+            href="https://www.facebook.com/won-games"
+            target="_blank"
+            rel="noopenner, noreferrer"
+          >
+            Facebook
+          </a>
+        </nav>
+      </S.Column>
+
+      <S.Column>
+        <Heading color="black" lineColor="secondary" lineBottom size="small">
+          Links
+        </Heading>
+
+        <nav aria-labelledby="footer resources">
+          <Link href="/">
+            <a>Home</a>
+          </Link>
+          <Link href="/games">
+            <a>Store</a>
+          </Link>
+          <Link href="/search">
+            <a>Buscar</a>
+          </Link>
+        </nav>
+      </S.Column>
+
+      <S.Column>
+        <Heading color="black" lineColor="secondary" lineBottom size="small">
+          Localização
+        </Heading>
+        <span>Loren Ipsum dolor sit.</span>
+        <span>Loren Ipsum</span>
+        <span>Lorem, ipsum dolor.</span>
+      </S.Column>
+
+      <S.Copyright>Won Games 2020 © Todos os direitos reservados.</S.Copyright>
+    </S.Content>
   </S.Wrapper>
 )
 
