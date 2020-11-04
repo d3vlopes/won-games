9b5b839973196339eaa09a5a77fe2a0209c32e33
Leandro Lopes <leandrolopes@DESKTOP-9QDA1FV.localdomain>
2020-11-04 17:28:59 -0300

    Testing headigns on Footer component

--- src/components/Footer/test.tsx
+++ src/components/Footer/test.tsx
@@ -1,13 +1,26 @@
-import { render, screen } from '@testing-library/react'
+import { screen } from '@testing-library/react'
+import { renderWithTheme } from 'utils/tests/helpers'
 
 import Footer from '.'
 
 describe('<Footer />', () => {
-  it('should render the heading', () => {
-    const { container } = render(<Footer />)
+  it('should render 4 topics', () => {
+    // Verificar se tem:
+    // contato
+    // siga-nos
+    // links
+    // endereço
 
-    expect(screen.getByRole('heading', { name: /Footer/i })).toBeInTheDocument()
-
-    expect(container.firstChild).toMatchSnapshot()
+    renderWithTheme(<Footer />)
+    expect(
+      screen.getByRole('heading', { name: /contato/i })
+    ).toBeInTheDocument()
+    expect(
+      screen.getByRole('heading', { name: /siga-nos/i })
+    ).toBeInTheDocument()
+    expect(screen.getByRole('heading', { name: /links/i })).toBeInTheDocument()
+    expect(
+      screen.getByRole('heading', { name: /endereço/i })
+    ).toBeInTheDocument()
   })
 })
