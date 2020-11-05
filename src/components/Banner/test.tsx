8e1b03def8d91c0fddc2e0ad538e3c6a9e505a5a
Leandro Lopes <leandrolopes@DESKTOP-9QDA1FV.localdomain>
2020-11-05 17:10:27 -0300

    Initial tests on Banner component

--- src/components/Banner/test.tsx
+++ src/components/Banner/test.tsx
@@ -1,12 +1,31 @@
-import { render, screen } from '@testing-library/react'
+import { screen } from '@testing-library/react'
+import { renderWithTheme } from 'utils/tests/helpers'
 
 import Banner from '.'
 
+const props = {
+  img: 'https://source.unsplash.com/user/willianjusten/1042x580',
+  title: 'Defy death',
+  subtitle: '<p>Play the new <strong>CrashLands</strong> season',
+  buttonLabel: 'Comprar',
+  buttonLink: '/games/defy-death'
+}
+
 describe('<Banner />', () => {
   it('should render the heading', () => {
-    const { container } = render(<Banner />)
+    const { container } = renderWithTheme(<Banner {...props} />)
+    // Verificar se o title está sendo passado x
+    // Verificar se o subitle está sendo passado
+    // Verificar se a imagem está sendo passado
+    expect(
+      screen.getByRole('heading', { name: /Defy death/i })
+    ).toBeInTheDocument()
 
-    expect(screen.getByRole('heading', { name: /Banner/i })).toBeInTheDocument()
+    expect(
+      screen.getByRole('heading', { name: /Play the new CrashLands season/i })
+    ).toBeInTheDocument()
+
+    expect(screen.getByRole('img', { name: /Defy death/i }))
 
     expect(container.firstChild).toMatchSnapshot()
   })
