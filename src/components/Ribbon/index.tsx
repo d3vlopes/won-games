9d22e5d0dd0df05384fb23bf8509c3218467c81b
Leandro Lopes <leandrolopes@DESKTOP-9QDA1FV.localdomain>
2020-11-06 15:50:15 -0300

    Create structure for Ribbon component

--- src/components/Ribbon/index.tsx
+++ src/components/Ribbon/index.tsx
@@ -1,8 +1,21 @@
 import * as S from './styles'
 
-const Ribbon = () => (
-  <S.Wrapper>
-    <h1>Ribbon</h1>
+export type RibbonColors = 'primary' | 'secondary'
+export type RibbonSize = 'normal' | 'small'
+
+export type RibbonProps = {
+  children: React.ReactNode
+  color?: RibbonColors
+  size?: RibbonSize
+}
+
+const Ribbon = ({
+  children,
+  color = 'primary',
+  size = 'normal'
+}: RibbonProps) => (
+  <S.Wrapper color={color} size={size}>
+    {children}
   </S.Wrapper>
 )
 
