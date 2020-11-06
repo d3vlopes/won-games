dbffac988152391d2556324c43e8c2c88deb8cc8
Leandro Lopes <leandrolopes@DESKTOP-9QDA1FV.localdomain>
2020-11-06 15:52:50 -0300

    Testing color and size prop on Ribbon component

--- src/components/Ribbon/test.tsx
+++ src/components/Ribbon/test.tsx
@@ -1,13 +1,46 @@
 import { render, screen } from '@testing-library/react'
+import { renderWithTheme } from 'utils/tests/helpers'
 
 import Ribbon from '.'
 
 describe('<Ribbon />', () => {
-  it('should render the heading', () => {
-    const { container } = render(<Ribbon />)
+  it('should render the text correctly', () => {
+    renderWithTheme(<Ribbon>Mais Vendidos </Ribbon>)
 
-    expect(screen.getByRole('heading', { name: /Ribbon/i })).toBeInTheDocument()
+    expect(screen.getByText(/Mais Vendidos/i)).toBeInTheDocument()
+  })
 
-    expect(container.firstChild).toMatchSnapshot()
+  it('should render the with the primary color', () => {
+    renderWithTheme(<Ribbon>Mais Vendidos </Ribbon>)
+
+    expect(screen.getByText(/Mais Vendidos/i)).toHaveStyle({
+      backgroundColor: '#F231A5'
+    })
+  })
+
+  it('should render the with the secondary color', () => {
+    renderWithTheme(<Ribbon color="secondary">Mais Vendidos </Ribbon>)
+
+    expect(screen.getByText(/Mais Vendidos/i)).toHaveStyle({
+      backgroundColor: '#3CD3C1'
+    })
+  })
+
+  it('should render with the normal size as default', () => {
+    renderWithTheme(<Ribbon>Mais Vendidos </Ribbon>)
+
+    expect(screen.getByText(/Mais Vendidos/i)).toHaveStyle({
+      height: '3.6rem',
+      fontSize: '1.4rem'
+    })
+  })
+
+  it('should render with the small size', () => {
+    renderWithTheme(<Ribbon size="small">Mais Vendidos </Ribbon>)
+
+    expect(screen.getByText(/Mais Vendidos/i)).toHaveStyle({
+      height: '2.6rem',
+      fontSize: '1.2rem'
+    })
   })
 })
