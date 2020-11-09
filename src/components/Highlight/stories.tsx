b4cb589e233fcd354aefd0a1dfb562e08615348b
Leandro Lopes <leandrolopes@DESKTOP-9QDA1FV.localdomain>
2020-11-09 15:48:30 -0300

    Add highlight initial structure

--- src/components/Highlight/stories.tsx
+++ src/components/Highlight/stories.tsx
@@ -1,9 +1,15 @@
 import { Story, Meta } from '@storybook/react/types-6-0'
-import Highlight from '.'
+import Highlight, { HiglightProps } from '.'
 
 export default {
   title: 'Highlight',
-  component: Highlight
+  component: Highlight,
+  args: {
+    title: 'Read Dead it’s back',
+    subtitle: 'Come see John’s new adventures',
+    buttonLabel: 'Comprar',
+    buttonLink: '/rdr2'
+  }
 } as Meta
 
-export const Default: Story = () => <Highlight />
+export const Default: Story<HiglightProps> = (args) => <Highlight {...args} />
