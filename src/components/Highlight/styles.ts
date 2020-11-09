c76b894914086ee5ee262a9e2f580dfa72ddf335
Leandro Lopes <leandrolopes@DESKTOP-9QDA1FV.localdomain>
2020-11-09 17:16:49 -0300

    Add style and adjusting for the alignment on Highlight component

--- src/components/Highlight/styles.ts
+++ src/components/Highlight/styles.ts
@@ -3,22 +3,40 @@ import media from 'styled-media-query'
 
 import { HiglightProps } from '.'
 
-type WrapperProps = Pick<HiglightProps, 'backgroundImage'>
+type WrapperProps = Pick<HiglightProps, 'backgroundImage' | 'alignment'>
+
+const wrapperModifiers = {
+  right: () => css`
+    grid-template-areas: 'floatimage content';
+    grid-template-columns: 1.3fr 2fr;
+
+    ${Content} {
+      text-align: right;
+    }
+  `,
+
+  left: () => css`
+    grid-template-areas: 'content floatimage';
+    grid-template-columns: 2fr 1.3fr;
+
+    ${Content} {
+      text-align: left;
+    }
+
+    ${FloatImage} {
+      justify-self: end;
+    }
+  `
+}
 
 export const Wrapper = styled.section<WrapperProps>`
-  ${({ backgroundImage }) => css`
+  ${({ backgroundImage, alignment }) => css`
     position: relative;
     background-image: url(${backgroundImage});
     background-position: center center;
     background-size: cover;
     height: 23rem;
     display: grid;
-    grid-template-areas: 'floatimage content';
-    grid-template-columns: 1.3fr 2fr;
-
-    ${media.greaterThan('medium')`
-      height: 32rem;
-    `}
 
     &::after {
       content: '';
@@ -27,6 +45,12 @@ export const Wrapper = styled.section<WrapperProps>`
       height: 100%;
       background-color: rgba(0, 0, 0, 0.6);
     }
+
+    ${media.greaterThan('medium')`
+      height: 32rem;
+    `}
+
+    ${wrapperModifiers[alignment!]()}
   `}
 `
 
@@ -48,7 +72,6 @@ export const Content = styled.div`
   ${({ theme }) => css`
     grid-area: content;
     z-index: ${theme.layers.base};
-    text-align: right;
     padding: ${theme.spacings.xsmall};
 
     ${media.greaterThan('medium')`
