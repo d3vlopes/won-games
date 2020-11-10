79074cfd83371925602eb4ec831ad293784f83b2
Leandro Lopes <leandrolopes@DESKTOP-9QDA1FV.localdomain>
2020-11-10 16:30:03 -0300

    Testing correctly rendered elements for Game Card component

--- src/components/GameCard/test.tsx
+++ src/components/GameCard/test.tsx
@@ -3,10 +3,32 @@ import { renderWithTheme } from 'utils/tests/helpers'
 
 import GameCard from '.'
 
-describe('<GameCard />', () => {
-  it('should render the heading', () => {
-    renderWithTheme(<GameCard />)
+const props = {
+  title: 'Population Zero',
+  developer: 'Rockstar Games',
+  img: 'https://source.unsplash.com/user/willianjusten/300x140',
+  price: 'R$ 235,00'
+}
 
-    expect(screen.getByRole('heading', { name: /GameCard/i })).toBeInTheDocument()
+describe('<GameCard />', () => {
+  it('should render correctly', () => {
+    renderWithTheme(<GameCard {...props} />)
+
+    expect(
+      screen.getByRole('heading', { name: props.title })
+    ).toBeInTheDocument()
+
+    expect(
+      screen.getByRole('heading', { name: props.developer })
+    ).toBeInTheDocument()
+
+    expect(screen.getByRole('img', { name: props.title })).toHaveAttribute(
+      'src',
+      props.img
+    )
+
+    expect(
+      screen.getByLabelText(/adicionar aos favoritos/i)
+    ).toBeInTheDocument()
   })
 })
