4edf17f95e71f2c715f3535a16ae6684cb1fee8b
Leandro Lopes <leandrolopes@DESKTOP-9QDA1FV.localdomain>
2020-11-10 16:28:25 -0300

    Creating initial styles structure for GameCard component

--- src/components/GameCard/styles.ts
+++ src/components/GameCard/styles.ts
@@ -1,7 +1,30 @@
 import styled, { css } from 'styled-components'
 
-export const Wrapper = styled.main`
-  ${({ theme }) => css`
+export const Wrapper = styled.article`
+  ${({ theme }) => css``}
+`
+export const ImageBox = styled.div`
+  ${({ theme }) => css``}
+`
+export const Info = styled.div`
+  ${({ theme }) => css``}
+`
+export const Title = styled.h3`
+  ${({ theme }) => css``}
+`
 
-  `}
+export const Developer = styled.h4`
+  ${({ theme }) => css``}
+`
+
+export const FavButton = styled.div`
+  ${({ theme }) => css``}
+`
+
+export const BuyBox = styled.div`
+  ${({ theme }) => css``}
+`
+
+export const Price = styled.div`
+  ${({ theme }) => css``}
 `
