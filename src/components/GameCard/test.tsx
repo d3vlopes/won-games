32687ba7f8b60cc494911669d16c297fffc0e317
Leandro Lopes <leandrolopes@DESKTOP-9QDA1FV.localdomain>
2020-11-10 17:17:44 -0300

    Testing price and styles for promotionalPrice on GameCard component

--- src/components/GameCard/test.tsx
+++ src/components/GameCard/test.tsx
@@ -1,4 +1,6 @@
 import { screen } from '@testing-library/react'
+
+import theme from 'styles/theme'
 import { renderWithTheme } from 'utils/tests/helpers'
 
 import GameCard from '.'
@@ -30,5 +32,33 @@ describe('<GameCard />', () => {
     expect(
       screen.getByLabelText(/adicionar aos favoritos/i)
     ).toBeInTheDocument()
+  })
+
+  it('should render price in label', () => {
+    // renderizar o componente
+    renderWithTheme(<GameCard {...props} />)
+
+    const price = screen.getByText('R$ 235,00')
+
+    // preço não tenha line-through
+    expect(price).not.toHaveStyle({ textDecoration: 'line-through' })
+    // preço não tenha cor cinza
+    expect(price).toHaveStyle({ color: theme.colors.white })
+    // preço tenha o background secundario
+    expect(price).toHaveStyle({ backgroundColor: theme.colors.secondary })
+  })
+
+  it('should render a line-through in price when promotional ', () => {
+    // renderizar o componente (COm promotionalPrice) 200 reaus 15 reais
+    renderWithTheme(<GameCard {...props} promotionalPrice="R$ 200,00" />)
+
+    // preço veho com line-through
+    expect(screen.getByText('R$ 235,00')).toHaveStyle({
+      textDecoration: 'line-through'
+    })
+    // preço novo não vai ter line-through
+    expect(screen.getByText('R$ 200,00')).not.toHaveStyle({
+      textDecoration: 'line-through'
+    })
   })
 })
