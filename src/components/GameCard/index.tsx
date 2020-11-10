cec9efb4216f817791952bc9e0fb22bb1db4b964
Leandro Lopes <leandrolopes@DESKTOP-9QDA1FV.localdomain>
2020-11-10 16:27:34 -0300

    Creating initial structure of the GameCard component

--- src/components/GameCard/index.tsx
+++ src/components/GameCard/index.tsx
@@ -1,8 +1,38 @@
+import {
+  FavoriteBorder,
+  AddShoppingCart
+} from '@styled-icons/material-outlined'
+
+import Button from 'components/Button'
+
 import * as S from './styles'
 
-const GameCard = () => (
+export type GameCardProps = {
+  title: string
+  developer: string
+  img: string
+  price: string
+}
+
+const GameCard = ({ title, developer, img, price }: GameCardProps) => (
   <S.Wrapper>
-    <h1>GameCard</h1>
+    <S.ImageBox>
+      <img src={img} alt={title} />
+    </S.ImageBox>
+
+    <S.Info>
+      <S.Title>{title}</S.Title>
+      <S.Developer>{developer}</S.Developer>
+    </S.Info>
+
+    <S.FavButton role="button">
+      <FavoriteBorder aria-label="Adicionar aos favoritos" />
+    </S.FavButton>
+
+    <S.BuyBox>
+      <S.Price>{price}</S.Price>
+      <Button icon={<AddShoppingCart />} size="small" />
+    </S.BuyBox>
   </S.Wrapper>
 )
 
