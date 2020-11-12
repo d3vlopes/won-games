13bebe6ea0e3445b6b902bbc513b46b2bbee3d39
Leandro Lopes <leandrolopes@DESKTOP-9QDA1FV.localdomain>
2020-11-12 16:25:27 -0300

    Testing BannerSlider component

--- src/components/BannerSlider/test.tsx
+++ src/components/BannerSlider/test.tsx
@@ -1,12 +1,48 @@
+import 'match-media-mock'
 import { screen } from '@testing-library/react'
-import { renderWithTheme } from 'utils/tests/helpers'
 
+import { renderWithTheme } from 'utils/tests/helpers'
 import BannerSlider from '.'
+
+const items = [
+  {
+    img: 'https://source.unsplash.com/user/willianjusten/1042x580',
+    title: 'Defy death 1',
+    subtitle: '<p>Play the new <strong>CrashLands</strong> season',
+    buttonLabel: 'Buy now',
+    buttonLink: '/games/defy-death',
+    ribbon: 'Bestselling'
+  },
+  {
+    img: 'https://source.unsplash.com/user/willianjusten/1042x582',
+    title: 'Defy death 2',
+    subtitle: '<p>Play the new <strong>CrashLands</strong> season',
+    buttonLabel: 'Buy now',
+    buttonLink: '/games/defy-death'
+  }
+]
 
 describe('<BannerSlider />', () => {
   it('should render the heading', () => {
-    renderWithTheme(<BannerSlider />)
+    const { container } = renderWithTheme(<BannerSlider items={items} />)
 
-    expect(screen.getByRole('heading', { name: /BannerSlider/i })).toBeInTheDocument()
+    expect(container.querySelector('.slick-vertical')).toBeInTheDocument()
+  })
+
+  it('should render with 1 active item', () => {
+    const { container } = renderWithTheme(<BannerSlider items={items} />)
+
+    // Verifica se tem 2 items
+    expect(container.querySelectorAll('.slick-slide')).toHaveLength(2)
+    // Verifica se tem apenas um li com a classe active
+    expect(container.querySelectorAll('li.slick-active')).toHaveLength(1)
+    // Verifica se o title do item[0] está aparecendo
+    expect(
+      screen.getByRole('heading', { name: /defy death 1/i, hidden: false })
+    ).toBeInTheDocument()
+    // Verifica se o title do item[1] não está aparecendo
+    expect(
+      screen.getByRole('heading', { name: /defy death 2/i, hidden: true })
+    ).toBeInTheDocument()
   })
 })
