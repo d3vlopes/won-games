038e5bf7bfb55048721b06041952644ccc7c4b81
Leandro Lopes <leandrolopes@DESKTOP-9QDA1FV.localdomain>
2020-11-12 16:39:24 -0300

    Generation of the GameCardSlider component structure

---
+++ src/components/GameCardSlider/test.tsx
@@ -0,0 +1,12 @@
+import { screen } from '@testing-library/react'
+import { renderWithTheme } from 'utils/tests/helpers'
+
+import GameCardSlider from '.'
+
+describe('<GameCardSlider />', () => {
+  it('should render the heading', () => {
+    renderWithTheme(<GameCardSlider />)
+
+    expect(screen.getByRole('heading', { name: /GameCardSlider/i })).toBeInTheDocument()
+  })
+})
