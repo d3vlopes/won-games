168c3c98931e888a2ba2abbd000caf2ade2c4fee
Leandro Lopes <leandrolopes@DESKTOP-9QDA1FV.localdomain>
2020-11-17 16:17:56 -0300

    Create mock for Highlight component

---
+++ src/components/Highlight/mock.ts
@@ -0,0 +1,7 @@
+export default {
+  title: 'Read Dead it’s back',
+  subtitle: 'Come see John’s new adventures',
+  backgroundImage: '/img/red-dead-img.png',
+  buttonLabel: 'Comprar',
+  buttonLink: '/rdr2'
+}
--- src/components/Highlight/stories.tsx
+++ src/components/Highlight/stories.tsx
@@ -1,16 +1,12 @@
 import { Story, Meta } from '@storybook/react/types-6-0'
 import Highlight, { HiglightProps } from '.'
 
+import item from './mock'
+
 export default {
   title: 'Highlight',
   component: Highlight,
-  args: {
-    title: 'Read Dead it’s back',
-    subtitle: 'Come see John’s new adventures',
-    backgroundImage: '/img/red-dead-img.png',
-    buttonLabel: 'Comprar',
-    buttonLink: '/rdr2'
-  }
+  args: { ...item }
 } as Meta
 
 export const Default: Story<HiglightProps> = (args) => (
