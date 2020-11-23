e7b48674813717579bbc37aeb7d1206e6c0110cd
Leandro Lopes <leandrolopes@DESKTOP-9QDA1FV.localdomain>
2020-11-23 17:25:54 -0300

    Add onChange for storybook of Checkbox component

--- src/components/Checkbox/stories.tsx
+++ src/components/Checkbox/stories.tsx
@@ -3,7 +3,11 @@ import Checkbox, { CheckboxProps } from '.'
 
 export default {
   title: 'Checkbox',
-  component: Checkbox
+  component: Checkbox,
+  argTypes: {
+    // Retorna a função na aba action
+    onCheck: { action: 'checked' }
+  }
 } as Meta
 
 export const Default: Story<CheckboxProps> = (args) => <Checkbox {...args} />
