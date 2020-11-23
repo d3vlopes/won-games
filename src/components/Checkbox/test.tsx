cc0622087bf53dc43f51945550cffdd599a62e3d
Leandro Lopes <leandrolopes@DESKTOP-9QDA1FV.localdomain>
2020-11-23 15:37:49 -0300

    Generation of the Checkbox component structure

---
+++ src/components/Checkbox/test.tsx
@@ -0,0 +1,12 @@
+import { screen } from '@testing-library/react'
+import { renderWithTheme } from 'utils/tests/helpers'
+
+import Checkbox from '.'
+
+describe('<Checkbox />', () => {
+  it('should render the heading', () => {
+    renderWithTheme(<Checkbox />)
+
+    expect(screen.getByRole('heading', { name: /Checkbox/i })).toBeInTheDocument()
+  })
+})
