8163be3cf2d4b76caba337dfc08816ddd7782d35
Leandro Lopes <leandro-lopes46@hotmail.com>
2020-11-30 23:17:07 -0300

    Initial testings for Auth template

--- src/templates/Auth/test.tsx
+++ src/templates/Auth/test.tsx
@@ -4,9 +4,36 @@ import { renderWithTheme } from 'utils/tests/helpers'
 import Auth from '.'
 
 describe('<Auth />', () => {
-  it('should render the heading', () => {
-    renderWithTheme(<Auth />)
+  it('should render all components and children', () => {
+    renderWithTheme(
+      <Auth title="Auth Title">
+        <input type="text" />
+      </Auth>
+    )
 
-    expect(screen.getByRole('heading', { name: /Auth/i })).toBeInTheDocument()
+    // Verifica se tem as logos
+    expect(screen.getAllByRole('img', { name: /won games/i })).toHaveLength(2)
+
+    // Verifica o heading principal do banner
+    expect(
+      screen.getByRole('heading', {
+        name: /Seus jogos favoritos em um único lugar/i
+      })
+    ).toBeInTheDocument()
+
+    // Verifica se tem o subtitle
+    expect(
+      screen.getByRole('heading', {
+        name: /WON é a melhor e mais completa plataforma de jogos./i
+      })
+    ).toBeInTheDocument()
+
+    // Verifica se tem o title do content
+    expect(
+      screen.getByRole('heading', { name: /Auth Title/i })
+    ).toBeInTheDocument()
+
+    // Verifica se o children tá sendo renderizado
+    expect(screen.getByRole('textbox')).toBeInTheDocument()
   })
 })
