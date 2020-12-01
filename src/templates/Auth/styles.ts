ffb2c4247b634ce3bbe58d076f0f7734875b943b
Leandro Lopes <leandro-lopes46@hotmail.com>
2020-11-30 23:12:12 -0300

    Create the style structure for the Auth template

--- src/templates/Auth/styles.ts
+++ src/templates/Auth/styles.ts
@@ -1,7 +1,13 @@
 import styled, { css } from 'styled-components'
 
 export const Wrapper = styled.main`
-  ${({ theme }) => css`
-
-  `}
+  ${({ theme }) => css``}
 `
+
+export const BannerBlock = styled.div``
+
+export const Subtitle = styled.h3``
+
+export const Footer = styled.p``
+
+export const Content = styled.div``
