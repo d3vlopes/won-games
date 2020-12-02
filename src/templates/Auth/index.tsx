757640246473e29eb054b10d39981f1fa0c7962a
Leandro Lopes <leandro-lopes46@hotmail.com>
2020-12-02 00:21:37 -0300

    Add ID on Logos components of the Auth template

--- src/templates/Auth/index.tsx
+++ src/templates/Auth/index.tsx
@@ -12,7 +12,7 @@ const Auth = ({ title, children }: AuthProps) => (
   <S.Wrapper>
     <S.BannerBlock>
       <S.BannerContent>
-        <Logo />
+        <Logo id="banner" />
 
         <div>
           <Heading>Seus jogos favoritos em um único lugar</Heading>
@@ -27,7 +27,7 @@ const Auth = ({ title, children }: AuthProps) => (
 
     <S.Content>
       <S.ContentWrapper>
-        <Logo color="black" size="large" />
+        <Logo id="content" color="black" size="large" />
         <Heading color="black" lineColor="secondary" lineLeft>
           {title}
         </Heading>
