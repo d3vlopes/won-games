656dc91676c7e560b3f041c3e0d783925207edfe
Leandro Lopes <leandro-lopes46@hotmail.com>
2020-12-03 20:28:44 -0300

    Refactoring the FormSignUp component index using the Form component

--- src/components/FormSignUp/index.tsx
+++ src/components/FormSignUp/index.tsx
@@ -1,13 +1,12 @@
 import Link from 'next/link'
 import { AccountCircle, Email, Lock } from '@styled-icons/material-outlined'
 
+import { FormWrapper, FormLink } from 'components/Form'
 import Button from 'components/Button'
 import TextField from 'components/TextField'
 
-import * as S from './styles'
-
 const FormSignUp = () => (
-  <S.Wrapper>
+  <FormWrapper>
     <form>
       <TextField
         name="name"
@@ -41,14 +40,14 @@ const FormSignUp = () => (
         Criar Conta
       </Button>
 
-      <S.FormLink>
+      <FormLink>
         Já tem conta?{' '}
         <Link href="/sign-in">
           <a>Entrar</a>
         </Link>
-      </S.FormLink>
+      </FormLink>
     </form>
-  </S.Wrapper>
+  </FormWrapper>
 )
 
 export default FormSignUp
