800dfebcdf31dba63e017960cbe2ea76bb55f990
Leandro Lopes <leandro-lopes46@hotmail.com>
2020-12-03 20:01:37 -0300

    Add structure of the FormSignUp component

--- src/components/FormSignUp/index.tsx
+++ src/components/FormSignUp/index.tsx
@@ -1,8 +1,53 @@
+import Link from 'next/link'
+import { AccountCircle, Email, Lock } from '@styled-icons/material-outlined'
+
+import Button from 'components/Button'
+import TextField from 'components/TextField'
+
 import * as S from './styles'
 
 const FormSignUp = () => (
   <S.Wrapper>
-    <h1>FormSignUp</h1>
+    <form>
+      <TextField
+        name="name"
+        placeholder="Name"
+        type="name"
+        icon={<AccountCircle />}
+      />
+
+      <TextField
+        name="email"
+        placeholder="Email"
+        type="email"
+        icon={<Email />}
+      />
+
+      <TextField
+        name="password"
+        placeholder="Password"
+        type="password"
+        icon={<Lock />}
+      />
+
+      <TextField
+        name="confirm-password"
+        placeholder="Confirm Password"
+        type="confirm-password"
+        icon={<Lock />}
+      />
+
+      <Button size="large" fullWidth>
+        Criar Conta
+      </Button>
+
+      <S.FormLink>
+        Já tem conta?{' '}
+        <Link href="/sign-in">
+          <a>Entrar</a>
+        </Link>
+      </S.FormLink>
+    </form>
   </S.Wrapper>
 )
 
