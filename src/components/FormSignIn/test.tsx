38a66ab8224754c63714d7077e1997947eb97fff
Leandro Lopes <leandro-lopes46@hotmail.com>
2020-12-03 19:24:43 -0300

    Add snapshot of FormSignIn component

--- src/components/FormSignIn/test.tsx
+++ src/components/FormSignIn/test.tsx
@@ -5,20 +5,34 @@ import FormSignIn from '.'
 
 describe('<FormSignIn />', () => {
   it('should render the form', () => {
-    renderWithTheme(<FormSignIn />)
+    const { container } = renderWithTheme(<FormSignIn />)
 
     // Verificar email
+    expect(screen.getByPlaceholderText(/email/i)).toBeInTheDocument()
 
     // Verificar password
+    expect(screen.getByPlaceholderText(/password/i)).toBeInTheDocument()
 
     // Verificar botão
+    expect(screen.getByRole('button', { name: /entrar/i })).toBeInTheDocument()
+
+    expect(container.parentElement).toMatchSnapshot()
   })
 
   it('should render the forgot password link', () => {
-    //
+    renderWithTheme(<FormSignIn />)
+
+    expect(
+      screen.getByRole('link', { name: /Esqueceu sua senha\?/i })
+    ).toBeInTheDocument()
   })
 
   it('should render the text and link to sign up', () => {
-    //
+    renderWithTheme(<FormSignIn />)
+
+    expect(screen.getByText(/Ainda não tem conta\?/i)).toBeInTheDocument()
+    expect(
+      screen.getByRole('link', { name: /criar conta/i })
+    ).toBeInTheDocument()
   })
 })
