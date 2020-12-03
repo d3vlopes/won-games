f77f622d9cbaf2d4a79caa743b38d82a8da41870
Leandro Lopes <leandro-lopes46@hotmail.com>
2020-12-03 20:02:20 -0300

    Add style of FormSignUp component

--- src/components/FormSignUp/styles.ts
+++ src/components/FormSignUp/styles.ts
@@ -1,7 +1,37 @@
 import styled, { css } from 'styled-components'
+import { darken } from 'polished'
+
+import * as TextFieldStyle from 'components/TextField/styles'
+import * as ButtonStyle from 'components/Button/styles'
 
 export const Wrapper = styled.main`
   ${({ theme }) => css`
+    ${TextFieldStyle.Wrapper} {
+      margin: ${theme.spacings.xxsmall};
+    }
 
+    ${ButtonStyle.Wrapper} {
+      margin: ${theme.spacings.medium} auto ${theme.spacings.xsmall};
+    }
+  `}
+`
+
+export const FormLink = styled.div`
+  ${({ theme }) => css`
+    font-size: ${theme.font.sizes.small};
+    color: ${theme.colors.black};
+    text-align: center;
+
+    a {
+      color: ${theme.colors.secondary};
+      text-decoration: none;
+      border-bottom: 0.1rem solid ${theme.colors.secondary};
+      transition: color, border, ${theme.transition.fast};
+
+      &:hover {
+        color: ${darken(0.1, theme.colors.secondary)};
+        border-bottom: 0.1rem solid ${darken(0.1, theme.colors.secondary)};
+      }
+    }
   `}
 `
