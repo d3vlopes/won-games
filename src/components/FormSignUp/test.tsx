94a2957bf17637ff445a5747df5c09538af6dff7
Leandro Lopes <leandro-lopes46@hotmail.com>
2020-12-03 20:03:17 -0300

    Testing the FormSignUp component

--- src/components/FormSignUp/test.tsx
+++ src/components/FormSignUp/test.tsx
@@ -4,9 +4,25 @@ import { renderWithTheme } from 'utils/tests/helpers'
 import FormSignUp from '.'
 
 describe('<FormSignUp />', () => {
-  it('should render the heading', () => {
+  it('should render the form', () => {
+    const { container } = renderWithTheme(<FormSignUp />)
+
+    expect(screen.getByPlaceholderText(/name/i)).toBeInTheDocument()
+    expect(screen.getByPlaceholderText(/email/i)).toBeInTheDocument()
+    expect(screen.getByPlaceholderText('Password')).toBeInTheDocument()
+    expect(screen.getByPlaceholderText('Confirm Password')).toBeInTheDocument()
+
+    expect(
+      screen.getByRole('button', { name: /Criar Conta/i })
+    ).toBeInTheDocument()
+
+    expect(container.firstChild).toMatchSnapshot()
+  })
+
+  it('should render text and link to sign in', () => {
     renderWithTheme(<FormSignUp />)
 
-    expect(screen.getByRole('heading', { name: /FormSignUp/i })).toBeInTheDocument()
+    expect(screen.getByRole('link', { name: /entrar/i })).toBeInTheDocument()
+    expect(screen.getByText(/Já tem conta\?/i)).toBeInTheDocument()
   })
 })
