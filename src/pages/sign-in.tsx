c2460fa439a550328d33fcf2eee4d0680fe3ef97
Leandro Lopes <leandro-lopes46@hotmail.com>
2020-12-04 15:01:19 -0300

    Add FormSignIn component on Sign-in page

--- src/pages/sign-in.tsx
+++ src/pages/sign-in.tsx
@@ -1,5 +1,10 @@
 import Auth from 'templates/Auth'
+import FormSignIn from 'components/FormSignIn'
 
 export default function SignIn() {
-  return <Auth title="Entrar">children</Auth>
+  return (
+    <Auth title="Entrar">
+      <FormSignIn />
+    </Auth>
+  )
 }
