0276fea64005bc0dd60d72d0245c3b3883db1668
Leandro Lopes <leandro-lopes46@hotmail.com>
2020-12-04 15:01:42 -0300

    Add FormSignUp component on Sign-up page

--- src/pages/sign-up.tsx
+++ src/pages/sign-up.tsx
@@ -1,5 +1,10 @@
 import Auth from 'templates/Auth'
+import FormSignUp from 'components/FormSignUp'
 
 export default function SignUp() {
-  return <Auth title="Criar Conta">children</Auth>
+  return (
+    <Auth title="Criar Conta">
+      <FormSignUp />
+    </Auth>
+  )
 }
