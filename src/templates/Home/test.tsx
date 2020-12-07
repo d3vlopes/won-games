6aec529f49461fc163c8a62a0cadbffb755170d8
Leandro Lopes <leandro-lopes46@hotmail.com>
2020-12-07 17:33:59 -0300

    Removing unnecessary tests from the Home template test

--- src/templates/Home/test.tsx
+++ src/templates/Home/test.tsx
@@ -20,24 +20,6 @@ const props = {
   freeHighlight: highlightMock
 }
 
-jest.mock('components/Menu', () => {
-  return {
-    __esModule: true,
-    default: function Mock() {
-      return <div data-testid="Mock Menu"></div>
-    }
-  }
-})
-
-jest.mock('components/Footer', () => {
-  return {
-    __esModule: true,
-    default: function Mock() {
-      return <div data-testid="Mock Footer"></div>
-    }
-  }
-})
-
 jest.mock('components/Showcase', () => {
   return {
     __esModule: true,
@@ -57,19 +39,13 @@ jest.mock('components/BannerSlider', () => {
 })
 
 describe('<Home />', () => {
-  it('should render menu and footer', () => {
+  it('should render banner and showcases', () => {
     renderWithTheme(<Home {...props} />)
-
-    // Verifica se o menu foi renderizado
-    expect(screen.getByTestId('Mock Menu')).toBeInTheDocument()
 
     // banner
     expect(screen.getByTestId('Mock Banner Slider')).toBeInTheDocument()
 
     // Verifica se o número de Showcase renderizado está correto
     expect(screen.getAllByTestId('Mock Showcase')).toHaveLength(5)
-
-    // Verifica se o footer foi renderizado
-    expect(screen.getByTestId('Mock Footer')).toBeInTheDocument()
   })
 })
