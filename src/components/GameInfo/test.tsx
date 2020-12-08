50afd2f37dd087f994d731a1c1501a6833ce3276
Leandro Lopes <leandro-lopes46@hotmail.com>
2020-12-08 17:02:33 -0300

    Initial tests for GameInfo component

--- src/components/GameInfo/test.tsx
+++ src/components/GameInfo/test.tsx
@@ -3,10 +3,39 @@ import { renderWithTheme } from 'utils/tests/helpers'
 
 import GameInfo from '.'
 
-describe('<GameInfo />', () => {
-  it('should render the heading', () => {
-    renderWithTheme(<GameInfo />)
+const props = {
+  title: 'My Game Title',
+  description: 'Game Description',
+  price: '210,00'
+}
 
-    expect(screen.getByRole('heading', { name: /GameInfo/i })).toBeInTheDocument()
+describe('<GameInfo />', () => {
+  it('should render game informations', () => {
+    renderWithTheme(<GameInfo {...props} />)
+
+    // Verifica o heading(title)
+    expect(
+      screen.getByRole('heading', { name: props.title })
+    ).toBeInTheDocument()
+
+    // Verifica a description
+    expect(screen.getByText(props.description)).toBeInTheDocument()
+
+    // Verifica o price
+    expect(screen.getByText(/r\$210,00/i)).toBeInTheDocument()
+  })
+
+  it('should render buttons', () => {
+    renderWithTheme(<GameInfo {...props} />)
+
+    // Verifica o button add to cart
+    expect(
+      screen.getByRole('button', { name: /adicionar ao carrinho/i })
+    ).toBeInTheDocument()
+
+    // Verifica o button wishlist
+    expect(
+      screen.getByRole('button', { name: /favoritos/i })
+    ).toBeInTheDocument()
   })
 })
