4289564694e1f3d5ef1d9993cedb37b1d0d48f42
Leandro Lopes <leandro-lopes46@hotmail.com>
2020-12-08 21:09:33 -0300

    Add news config for storybook of GameInfo component

--- src/components/GameInfo/index.tsx
+++ src/components/GameInfo/index.tsx
@@ -24,7 +24,7 @@ const GameInfo = ({ title, description, price }: GameInfoProps) => (
 
     <S.ButtonsWrapper>
       <Button size="large" icon={<AddShoppingCart />}>
-        Adicionar ao carrinho
+        Adicionar
       </Button>
 
       <Button icon={<HeartOutline />} size="large" minimal>
--- src/components/GameInfo/stories.tsx
+++ src/components/GameInfo/stories.tsx
@@ -1,9 +1,21 @@
 import { Story, Meta } from '@storybook/react/types-6-0'
-import GameInfo from '.'
+
+import GameInfo, { GameInfoProps } from '.'
+import mockGames from './mock'
 
 export default {
   title: 'GameInfo',
-  component: GameInfo
+  component: GameInfo,
+  parameters: {
+    backgrounds: {
+      default: 'won-dark'
+    }
+  },
+  args: mockGames
 } as Meta
 
-export const Default: Story = () => <GameInfo />
+export const Default: Story<GameInfoProps> = (args) => (
+  <div style={{ maxWidth: '144rem', padding: '1.5rem' }}>
+    <GameInfo {...args} />
+  </div>
+)
