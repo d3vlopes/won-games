0760b446ab50c5add000293a7d75cefe8da7c615
Leandro Lopes <leandro-lopes46@hotmail.com>
2020-12-13 14:46:33 -0300

    Testing  prop rating of GameDetails component

--- src/components/GameDetails/test.tsx
+++ src/components/GameDetails/test.tsx
@@ -6,7 +6,8 @@ import GameDetails, { GameDetailsProps } from '.'
 const props: GameDetailsProps = {
   developer: 'Different Tales',
   platforms: ['windows', 'mac', 'linux'],
-  releaseDate: '2020-11-21T23:00:00'
+  releaseDate: '2020-11-21T23:00:00',
+  rating: 'BR0'
 }
 
 describe('<GameDetails />', () => {
@@ -42,6 +43,18 @@ describe('<GameDetails />', () => {
     expect(screen.getByRole('img', { name: /linux/i })).toBeInTheDocument()
     expect(screen.getByRole('img', { name: /windows/i })).toBeInTheDocument()
     expect(screen.getByRole('img', { name: /mac/i })).toBeInTheDocument()
+  })
+
+  it('should render free rating when BR0', () => {
+    renderWithTheme(<GameDetails {...props} />)
+
+    expect(screen.getByText(/gratuito/i)).toBeInTheDocument()
+  })
+
+  it('should render 18+ rating when BR0', () => {
+    renderWithTheme(<GameDetails {...props} rating="BR18" />)
+
+    expect(screen.getByText(/18\+/i)).toBeInTheDocument()
   })
 
   it('should render the formated date', () => {
