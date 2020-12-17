6947609bd5a0ee43ac52b23c5331571d6243b3b9
Leandro Lopes <leandro-lopes46@hotmail.com>
2020-12-17 20:07:02 -0300

    Add Gallery on Game template

--- src/templates/Game/index.tsx
+++ src/templates/Game/index.tsx
@@ -1,21 +1,29 @@
 import Base from 'templates/Base'
 
+import MediaMatch from 'components/MediaMatch'
+
 import GameInfo, { GameInfoProps } from 'components/GameInfo'
+import Gallery, { GalleryImageProps } from 'components/Gallery'
 
 import * as S from './styles'
 
 export type GameTemplateProps = {
   cover: string
   gameInfo: GameInfoProps
+  gallery?: GalleryImageProps[]
 }
 
-const Game = ({ cover, gameInfo }: GameTemplateProps) => (
+const Game = ({ cover, gameInfo, gallery }: GameTemplateProps) => (
   <Base>
     <S.Cover src={cover} role="image" aria-label="cover" />
     <S.Main>
       <S.SectionGameInfo>
         <GameInfo {...gameInfo} />
       </S.SectionGameInfo>
+
+      <MediaMatch greaterThan="medium">
+        {!!gallery && <Gallery items={gallery} />}
+      </MediaMatch>
     </S.Main>
   </Base>
 )
