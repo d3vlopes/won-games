cc71b2f294df10f437ac11a18fb9882e42ff85d4
Leandro Lopes <leandro-lopes46@hotmail.com>
2020-12-18 09:17:54 -0300

    Add publisher prop on GameDetails component

--- src/components/GameDetails/index.tsx
+++ src/components/GameDetails/index.tsx
@@ -13,6 +13,7 @@ type Rating = 'BR0' | 'BR10' | 'BR12' | 'BR14' | 'BR16' | 'BR18'
 
 export type GameDetailsProps = {
   developer: string
+  publisher: string
   platforms: Platform[]
   releaseDate: string
   rating: Rating
@@ -21,6 +22,7 @@ export type GameDetailsProps = {
 
 const GameDetails = ({
   developer,
+  publisher,
   releaseDate,
   platforms,
   rating,
@@ -65,7 +67,7 @@ const GameDetails = ({
 
         <S.Block>
           <S.Label>Distribuidora</S.Label>
-          <S.Description>2K</S.Description>
+          <S.Description>{publisher}</S.Description>
         </S.Block>
 
         <S.Block>
