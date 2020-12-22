21c3a2fbadcbe9f235812d618ba562cbf0aae3dc
Leandro Lopes <leandro-lopes46@hotmail.com>
2020-12-22 09:39:06 -0300

    Testing render correctly for Grid component

--- src/components/Grid/test.tsx
+++ src/components/Grid/test.tsx
@@ -1,12 +1,24 @@
-import { screen } from '@testing-library/react'
 import { renderWithTheme } from 'utils/tests/helpers'
 
-import Grid from '.'
+import { Grid } from '.'
 
 describe('<Grid />', () => {
-  it('should render the heading', () => {
-    renderWithTheme(<Grid />)
+  it('should render correctly', () => {
+    const { container } = renderWithTheme(<Grid>Children</Grid>)
 
-    expect(screen.getByRole('heading', { name: /Grid/i })).toBeInTheDocument()
+    expect(container.firstChild).toMatchInlineSnapshot(`
+      .c0 {
+        display: grid;
+        grid-template-columns: repeat(auto-fill,minmax(25rem,1fr));
+        grid-gap: 3.2rem;
+        margin: 3.2rem 0;
+      }
+
+      <div
+        class="c0"
+      >
+        Children
+      </div>
+    `)
   })
 })
