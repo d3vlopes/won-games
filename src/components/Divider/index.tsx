e0098b55cbd5aacfab6ce8e8c2c3343c1500f4d8
Leandro Lopes <leandro-lopes46@hotmail.com>
2020-12-22 10:03:51 -0300

    Creating structure for Divider component

--- src/components/Divider/index.tsx
+++ src/components/Divider/index.tsx
@@ -1,7 +1,15 @@
-const Divider = () => (
-  <div>
-    <h1>Divider</h1>
-  </div>
-)
+import styled, { css } from 'styled-components'
+import media from 'styled-media-query'
 
-export default Divider
+export const Divider = styled.hr`
+  ${({ theme }) => css`
+    margin: ${theme.spacings.xxlarge} auto ${theme.spacings.medium};
+    height: 0.1rem;
+    background: rgba(181, 181, 181, 0.3);
+    border: 0;
+
+    ${media.greaterThan('medium')`
+      margin: calc(${theme.spacings.xxlarge} * 2.5) auto ${theme.spacings.xxlarge};
+    `}
+  `}
+`
