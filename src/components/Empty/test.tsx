cf6e2042d83f40bbe4a07c6f72f63ef62c735cb3
Leandro Lopes <leandro-lopes46@hotmail.com>
2020-12-22 10:52:13 -0300

    Initial tests of the Empty component

--- src/components/Empty/test.tsx
+++ src/components/Empty/test.tsx
@@ -3,10 +3,35 @@ import { renderWithTheme } from 'utils/tests/helpers'
 
 import Empty from '.'
 
-describe('<Empty />', () => {
-  it('should render the heading', () => {
-    renderWithTheme(<Empty />)
+const props = {
+  title: 'A simple title',
+  description: 'A simple description'
+}
 
-    expect(screen.getByRole('heading', { name: /Empty/i })).toBeInTheDocument()
+describe('<Empty />', () => {
+  it('should render correctly', () => {
+    renderWithTheme(<Empty {...props} hasLink />)
+
+    expect(
+      screen.getByRole('image', {
+        name: /um jogador em um sofá jogando videogame/i
+      })
+    ).toBeInTheDocument()
+
+    expect(screen.getByRole('heading', { name: /a simple title/i }))
+
+    expect(screen.getByText(/a simple description/i)).toBeInTheDocument()
+
+    expect(
+      screen.getByRole('link', { name: /voltar para a loja/i })
+    ).toHaveAttribute('href', '/')
+  })
+
+  it('should not render link when hasLink is not passed ', () => {
+    renderWithTheme(<Empty {...props} />)
+
+    expect(
+      screen.queryByRole('link', { name: /voltar para a loja/i })
+    ).not.toBeInTheDocument()
   })
 })
