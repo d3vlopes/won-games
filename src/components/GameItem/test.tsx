5a45f96ac82dc6e96afa80f055ccebb6fb77e006
Leandro Lopes <leandro-lopes46@hotmail.com>
2020-12-23 16:34:48 -0300

    Initial test of the GameItem component

--- src/components/GameItem/test.tsx
+++ src/components/GameItem/test.tsx
@@ -3,12 +3,27 @@ import { renderWithTheme } from 'utils/tests/helpers'
 
 import GameItem from '.'
 
-describe('<GameItem />', () => {
-  it('should render the heading', () => {
-    renderWithTheme(<GameItem />)
+const props = {
+  img: 'https://source.unsplash.com/user/willianjusten/151x70',
+  title: 'Red Dead Redemption 2',
+  price: 'R$ 215,00'
+}
 
+describe('<GameItem />', () => {
+  it('should render the item', () => {
+    renderWithTheme(<GameItem {...props} />)
+
+    // Verifica se o title foi renderizado
     expect(
-      screen.getByRole('heading', { name: /GameItem/i })
+      screen.getByRole('heading', { name: /red dead redemption 2/i })
     ).toBeInTheDocument()
+
+    // Verifica se a imagem foi renderizada
+    expect(
+      screen.getByRole('img', { name: /red dead redemption 2/i })
+    ).toHaveAttribute('src', props.img)
+
+    // Verifica se o price foi renderizado
+    expect(screen.getByText('R$ 215,00')).toBeInTheDocument()
   })
 })
