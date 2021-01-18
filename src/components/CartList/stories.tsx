bc021f6bb67d95cf3386b7ad60faf4040a54f9d1
Leandro Lopes <leandro-lopes46@hotmail.com>
2021-01-18 16:41:16 -0300

    Config storybook of CartList component

--- src/components/CartList/stories.tsx
+++ src/components/CartList/stories.tsx
@@ -1,9 +1,29 @@
 import { Story, Meta } from '@storybook/react/types-6-0'
-import CardList from '.'
+
+import CardList, { CartListProps } from '.'
+import mockItems from './mock'
 
 export default {
   title: 'CardList',
-  component: CardList
+  component: CardList,
+  args: {
+    items: mockItems,
+    total: 'R$ 330,00'
+  },
+  argTypes: {
+    items: {
+      type: ''
+    }
+  },
+  parameters: {
+    backgrounds: {
+      default: 'won-dark'
+    }
+  }
 } as Meta
 
-export const Default: Story = () => <CardList />
+export const Default: Story<CartListProps> = (args) => (
+  <div style={{ maxWidth: 800 }}>
+    <CardList {...args} />
+  </div>
+)
