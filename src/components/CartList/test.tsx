716ea22cb949d78b23ad83802ec6096880872eb1
Leandro Lopes <leandro-lopes46@hotmail.com>
2021-01-18 16:41:55 -0300

    Testing the CartList component

--- src/components/CartList/test.tsx
+++ src/components/CartList/test.tsx
@@ -2,13 +2,19 @@ import { screen } from '@testing-library/react'
 import { renderWithTheme } from 'utils/tests/helpers'
 
 import CardList from '.'
+import mockItems from './mock'
 
 describe('<CardList />', () => {
-  it('should render the heading', () => {
-    renderWithTheme(<CardList />)
+  it('should render the cart list', () => {
+    const { container } = renderWithTheme(
+      <CardList items={mockItems} total="R$ 330,00" />
+    )
 
-    expect(
-      screen.getByRole('heading', { name: /CardList/i })
-    ).toBeInTheDocument()
+    expect(screen.getAllByRole('heading')).toHaveLength(2)
+    expect(screen.getByText('R$ 330,00')).toHaveStyle({
+      color: '#F231A5'
+    })
+
+    expect(container.firstChild).toMatchSnapshot()
   })
 })
