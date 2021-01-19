444c8b6f6edafc6fa5c5b3a408ec6232c6f69201
Leandro Lopes <leandro-lopes46@hotmail.com>
2021-01-19 17:02:47 -0300

    Generation of the Cart template structure

---
+++ src/templates/Cart/test.tsx
@@ -0,0 +1,12 @@
+import { screen } from '@testing-library/react'
+import { renderWithTheme } from 'utils/tests/helpers'
+
+import Cart from '.'
+
+describe('<Cart />', () => {
+  it('should render the Cart', () => {
+    renderWithTheme(<Cart />)
+
+    expect(screen.getByRole('heading', { name: /Cart/i })).toBeInTheDocument()
+  })
+})
