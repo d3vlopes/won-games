68358aecd81d224c9754c24c55eaf9bdfee19e6b
Leandro Lopes <leandro-lopes46@hotmail.com>
2021-01-19 17:58:50 -0300

    Creating structure of Cart template

--- src/templates/Cart/index.tsx
+++ src/templates/Cart/index.tsx
@@ -1,9 +1,53 @@
+import Base from 'templates/Base'
+
+import { Container } from 'components/Container'
+import { GameCardProps } from 'components/GameCard'
+import { HighlightProps } from 'components/Highlight'
+import { Divider } from 'components/Divider'
+import CartList, { CartListProps } from 'components/CartList'
+import PaymentOptions, { PaymentOptionsProps } from 'components/PaymentOptions'
+import Heading from 'components/Heading'
+import Showcase from 'components/Showcase'
+
 import * as S from './styles'
 
-const Cart = () => (
-  <S.Wrapper>
-    <h1>Cart</h1>
-  </S.Wrapper>
-)
+export type CartProps = {
+  recommendedGames: GameCardProps[]
+  recommendedHighlight: HighlightProps
+} & CartListProps &
+  Pick<PaymentOptionsProps, 'cards'>
+
+const Cart = ({
+  recommendedGames,
+  recommendedHighlight,
+  items,
+  total,
+  cards
+}: CartProps) => {
+  const handlePayment = () => ({})
+
+  return (
+    <Base>
+      <Container>
+        <Heading lineLeft lineColor="secondary">
+          Meu carrinho
+        </Heading>
+
+        <S.Content>
+          <CartList items={items} total={total} />
+
+          <PaymentOptions cards={cards} handlePayment={handlePayment} />
+        </S.Content>
+        <Divider />
+      </Container>
+
+      <Showcase
+        title="Você pode gostar desses jogos"
+        games={recommendedGames}
+        highlight={recommendedHighlight}
+      />
+    </Base>
+  )
+}
 
 export default Cart
