df3c4b58924fc81dbe3624d86d29e8db46c867be
Leandro Lopes <leandro-lopes46@hotmail.com>
2021-01-20 09:15:18 -0300

    Creating structure of ProfileMenu component

--- src/components/ProfileMenu/index.tsx
+++ src/components/ProfileMenu/index.tsx
@@ -1,9 +1,43 @@
+import Link from 'next/link'
+import {
+  AccountCircle,
+  CreditCard,
+  FormatListBulleted,
+  ExitToApp
+} from '@styled-icons/material-outlined'
+
 import * as S from './styles'
 
 const ProfileMenu = () => (
-  <S.Wrapper>
-    <h1>ProfileMenu</h1>
-  </S.Wrapper>
+  <S.Nav>
+    <Link href="/profile/me" passHref>
+      <S.Link>
+        <AccountCircle size={24} />
+        <span>Meu perfil</span>
+      </S.Link>
+    </Link>
+
+    <Link href="/profile/cards" passHref>
+      <S.Link>
+        <CreditCard size={24} />
+        <span>Meus Cartões</span>
+      </S.Link>
+    </Link>
+
+    <Link href="/profile/orders" passHref>
+      <S.Link>
+        <FormatListBulleted size={24} />
+        <span>Meus pedidos</span>
+      </S.Link>
+    </Link>
+
+    <Link href="/logout" passHref>
+      <S.Link>
+        <ExitToApp size={24} />
+        <span>Sair</span>
+      </S.Link>
+    </Link>
+  </S.Nav>
 )
 
 export default ProfileMenu
