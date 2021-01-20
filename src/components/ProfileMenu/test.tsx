4683c4ca40a57f53482ca11a0f8434dee15096e2
Leandro Lopes <leandro-lopes46@hotmail.com>
2021-01-20 09:16:23 -0300

    Testing the ProfileMenu component

--- src/components/ProfileMenu/test.tsx
+++ src/components/ProfileMenu/test.tsx
@@ -5,10 +5,19 @@ import ProfileMenu from '.'
 
 describe('<ProfileMenu />', () => {
   it('should render the ProfileMenu', () => {
-    renderWithTheme(<ProfileMenu />)
+    const { container } = renderWithTheme(<ProfileMenu />)
 
     expect(
-      screen.getByRole('heading', { name: /ProfileMenu/i })
+      screen.getByRole('link', { name: /meu perfil/i })
     ).toBeInTheDocument()
+    expect(
+      screen.getByRole('link', { name: /meus cartões/i })
+    ).toBeInTheDocument()
+    expect(
+      screen.getByRole('link', { name: /meus pedidos/i })
+    ).toBeInTheDocument()
+    expect(screen.getByRole('link', { name: /sair/i })).toBeInTheDocument()
+
+    expect(container.firstChild).toMatchSnapshot()
   })
 })
