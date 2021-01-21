26258507bf3dc530a37dd1356e34f375f0b60b20
Leandro Lopes <leandro-lopes46@hotmail.com>
2021-01-21 16:51:36 -0300

    Creating structure of FormProfile component

--- src/components/FormProfile/index.tsx
+++ src/components/FormProfile/index.tsx
@@ -1,9 +1,49 @@
+import Button from 'components/Button'
+import Heading from 'components/Heading'
+import TextField from 'components/TextField'
+
 import * as S from './styles'
 
 const FormProfile = () => (
-  <S.Wrapper>
-    <h1>FormProfile</h1>
-  </S.Wrapper>
+  <>
+    <Heading lineBottom color="black" size="small">
+      Meu perfil
+    </Heading>
+
+    <S.Form>
+      <TextField
+        name="name"
+        placeholder="Nome"
+        label="Nome"
+        initialValue="John Doe"
+      />
+
+      <TextField
+        name="email"
+        type="email"
+        placeholder="Email"
+        initialValue="johndoe@gmail.com"
+        disabled
+        label="Email"
+      />
+
+      <TextField
+        name="password"
+        type="password"
+        placeholder="Sua senha atual"
+        label="Senha"
+      />
+
+      <TextField
+        name="new_password"
+        type="password"
+        placeholder="Nova senha"
+        label="Nova senha"
+      />
+
+      <Button size="large">Salvar</Button>
+    </S.Form>
+  </>
 )
 
 export default FormProfile
