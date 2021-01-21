3431d2e8edb56e47b45507dd05ccc70049628afe
Leandro Lopes <leandro-lopes46@hotmail.com>
2021-01-21 16:53:22 -0300

    Testing the FormProfile component

--- src/components/FormProfile/test.tsx
+++ src/components/FormProfile/test.tsx
@@ -4,11 +4,19 @@ import { renderWithTheme } from 'utils/tests/helpers'
 import FormProfile from '.'
 
 describe('<FormProfile />', () => {
-  it('should render the FormProfile', () => {
+  it('should render the profile form', () => {
     renderWithTheme(<FormProfile />)
 
     expect(
-      screen.getByRole('heading', { name: /FormProfile/i })
+      screen.getByRole('heading', { name: /meu perfil/i })
     ).toBeInTheDocument()
+
+    expect(screen.getByRole('textbox', { name: /nome/i })).toBeInTheDocument()
+    expect(screen.getByRole('textbox', { name: /email/i })).toBeInTheDocument()
+
+    expect(screen.getByPlaceholderText(/sua senha atual/i)).toBeInTheDocument()
+    expect(screen.getByPlaceholderText(/nova senha/i)).toBeInTheDocument()
+
+    expect(screen.getByRole('button', { name: /salvar/i })).toBeInTheDocument()
   })
 })
