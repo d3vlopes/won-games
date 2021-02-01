a9016b5a94370f694a8eb1bf5a4f095e77f640f2
Leandro Lopes <leandro-lopes46@hotmail.com>
2021-02-01 17:29:20 -0300

    Initial testings the of ExploreSidebar component

--- src/components/ExploreSidebar/test.tsx
+++ src/components/ExploreSidebar/test.tsx
@@ -4,11 +4,34 @@ import { renderWithTheme } from 'utils/tests/helpers'
 import ExploreSidebar from '.'
 
 describe('<ExploreSidebar />', () => {
-  it('should render the ExploreSidebar', () => {
+  it('should render headings', () => {
+    renderWithTheme(<ExploreSidebar />)
+
+    expect(screen.getByRole('heading', { name: /preço/i })).toBeInTheDocument()
+    expect(
+      screen.getByRole('heading', { name: /ordenar por/i })
+    ).toBeInTheDocument()
+    expect(
+      screen.getByRole('heading', { name: /sistema/i })
+    ).toBeInTheDocument()
+    expect(screen.getByRole('heading', { name: /gênero/i })).toBeInTheDocument()
+  })
+
+  it('should render inputs', () => {
     renderWithTheme(<ExploreSidebar />)
 
     expect(
-      screen.getByRole('heading', { name: /ExploreSidebar/i })
+      screen.getByRole('checkbox', { name: /under \$50/i })
     ).toBeInTheDocument()
+
+    expect(
+      screen.getByRole('radio', { name: /Do menor para o maior/i })
+    ).toBeInTheDocument()
+  })
+
+  it('should render the filter button', () => {
+    renderWithTheme(<ExploreSidebar />)
+
+    expect(screen.getByRole('button', { name: /filtrar/i })).toBeInTheDocument()
   })
 })
