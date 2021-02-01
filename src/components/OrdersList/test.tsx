9108c61e7da2b0a6520647e4db166c106805e41e
Leandro Lopes <leandro-lopes46@hotmail.com>
2021-02-01 16:41:21 -0300

    Testing the OrdersList component

--- src/components/OrdersList/test.tsx
+++ src/components/OrdersList/test.tsx
@@ -2,13 +2,36 @@ import { screen } from '@testing-library/react'
 import { renderWithTheme } from 'utils/tests/helpers'
 
 import OrdersList from '.'
+import mock from './mock'
+
+jest.mock('components/Empty', () => ({
+  __esModule: true,
+  default: function Mock() {
+    return <div data-testid="Mock Empty" />
+  }
+}))
+
+jest.mock('components/GameItem', () => ({
+  __esModule: true,
+  default: function Mock({ children }: { children: React.ReactNode }) {
+    return <div data-testid="Mock GameItem">{children}</div>
+  }
+}))
 
 describe('<OrdersList />', () => {
   it('should render the OrdersList', () => {
-    renderWithTheme(<OrdersList />)
+    renderWithTheme(<OrdersList items={mock} />)
 
     expect(
-      screen.getByRole('heading', { name: /OrdersList/i })
+      screen.getByRole('heading', { name: /meus pedidos/i })
     ).toBeInTheDocument()
+
+    expect(screen.getAllByTestId('Mock GameItem')).toHaveLength(2)
+  })
+
+  it('should render empty state', () => {
+    renderWithTheme(<OrdersList />)
+
+    expect(screen.getByTestId('Mock Empty')).toBeInTheDocument()
   })
 })
