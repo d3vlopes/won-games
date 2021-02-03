125d0d882a9f97e687dbccc86b84c57f97b72eda
Leandro Lopes <leandro-lopes46@hotmail.com>
2021-02-03 18:47:01 -0300

    Testing the styles of ExploreSidebar component

--- src/components/ExploreSidebar/test.tsx
+++ src/components/ExploreSidebar/test.tsx
@@ -1,6 +1,9 @@
 import { screen } from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
+import { css } from 'styled-components'
 import { renderWithTheme } from 'utils/tests/helpers'
+
+import { Overlay } from './styles'
 
 import ExploreSidebar from '.'
 import items from './mock'
@@ -99,5 +102,30 @@ describe('<ExploreSidebar />', () => {
     userEvent.click(screen.getByRole('button', { name: /filtrar/i }))
 
     expect(onFilter).toBeCalledWith({ sort_by: 'high-to-low' })
+  })
+
+  it('should open/close sidebar when filtering on mobile ', () => {
+    const { container } = renderWithTheme(
+      <ExploreSidebar items={items} onFilter={jest.fn} />
+    )
+
+    const variant = {
+      media: '(max-width:768px)',
+      modifier: String(css`
+        ${Overlay}
+      `)
+    }
+
+    const Element = container.firstChild
+
+    expect(Element).not.toHaveStyleRule('opacity', '1', variant)
+
+    userEvent.click(screen.getByLabelText(/open filters/))
+
+    expect(Element).toHaveStyleRule('opacity', '1', variant)
+
+    userEvent.click(screen.getByLabelText(/close filters/))
+
+    expect(Element).not.toHaveStyleRule('opacity', '1', variant)
   })
 })
