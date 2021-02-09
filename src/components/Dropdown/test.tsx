03efd12fea1d2997a923104b4d628302e93a8434
Leandro Lopes <leandro-lopes46@hotmail.com>
2021-02-09 16:07:10 -0300

    Generation of the Dropdown component structure

---
+++ src/components/Dropdown/test.tsx
@@ -0,0 +1,14 @@
+import { screen } from '@testing-library/react'
+import { renderWithTheme } from 'utils/tests/helpers'
+
+import Dropdown from '.'
+
+describe('<Dropdown />', () => {
+  it('should render the Dropdown', () => {
+    renderWithTheme(<Dropdown />)
+
+    expect(
+      screen.getByRole('heading', { name: /Dropdown/i })
+    ).toBeInTheDocument()
+  })
+})
