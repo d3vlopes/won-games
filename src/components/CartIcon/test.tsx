4cf8f0b41bc21651bcb04fad675265908788bf61
Leandro Lopes <leandro-lopes46@hotmail.com>
2021-02-09 14:46:22 -0300

    Testing the CartIcon component

--- src/components/CartIcon/test.tsx
+++ src/components/CartIcon/test.tsx
@@ -4,11 +4,28 @@ import { renderWithTheme } from 'utils/tests/helpers'
 import CartIcon from '.'
 
 describe('<CartIcon />', () => {
-  it('should render the CartIcon', () => {
+  it('should render without badge', () => {
     renderWithTheme(<CartIcon />)
 
+    expect(screen.getByLabelText(/carrinho/i)).toBeInTheDocument()
     expect(
-      screen.getByRole('heading', { name: /CartIcon/i })
-    ).toBeInTheDocument()
+      screen.queryByLabelText(/items do carrinho/i)
+    ).not.toBeInTheDocument()
+  })
+
+  it('should render with badge', () => {
+    renderWithTheme(<CartIcon quantity={3} />)
+
+    expect(screen.getByLabelText(/items do carrinho/i)).toBeInTheDocument()
+    expect(screen.getByText(/3/)).toBeInTheDocument()
+  })
+
+  it('should render with badge only if has positive numbers', () => {
+    renderWithTheme(<CartIcon quantity={-1} />)
+
+    expect(
+      screen.queryByLabelText(/items do carrinho/i)
+    ).not.toBeInTheDocument()
+    expect(screen.queryByLabelText(/-1/)).not.toBeInTheDocument()
   })
 })
