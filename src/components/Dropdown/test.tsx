422541c2f0d5475b1e7fead34cbae1f34c81c6c1
Leandro Lopes <leandro-lopes46@hotmail.com>
2021-02-10 13:34:00 -0300

    Testing the Dropdown component

--- src/components/Dropdown/test.tsx
+++ src/components/Dropdown/test.tsx
@@ -1,15 +1,33 @@
-// import { screen } from '@testing-library/react'
+import { screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 import { renderWithTheme } from 'utils/tests/helpers'
 
 import Dropdown from '.'
 
-const props = {
-  title: 'Click Here',
-  children: 'Content'
-}
-
 describe('<Dropdown />', () => {
-  it('should render the Dropdown', () => {
-    renderWithTheme(<Dropdown {...props} />)
+  beforeEach(() => {
+    const title = <h1 aria-label="toggle dropdown">Click here</h1>
+
+    renderWithTheme(
+      <Dropdown title={title}>
+        <span>content</span>
+      </Dropdown>
+    )
+  })
+
+  it('should render title', () => {
+    expect(screen.getByLabelText(/toggle dropdown/)).toBeInTheDocument()
+  })
+
+  it('should handle open/close dropdown', () => {
+    const content = screen.getByText(/content/).parentElement!
+
+    expect(content).toHaveStyle({ opacity: 0 })
+    expect(content.getAttribute('aria-hidden')).toBe('true')
+
+    userEvent.click(screen.getByLabelText(/toggle dropdown/))
+
+    expect(content).toHaveStyle({ opacity: 1 })
+    expect(content.getAttribute('aria-hidden')).toBe('false')
   })
 })
