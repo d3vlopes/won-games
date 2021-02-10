d4cf5ec4b3908b67f98044505793c8536ec3f533
Leandro Lopes <leandro-lopes46@hotmail.com>
2021-02-10 14:22:42 -0300

    Initial testing the CartDropdown component

--- src/components/CartDropdown/test.tsx
+++ src/components/CartDropdown/test.tsx
@@ -1,14 +1,22 @@
 import { screen } from '@testing-library/react'
 import { renderWithTheme } from 'utils/tests/helpers'
 
+import items from 'components/CartList/mock'
+
 import CartDropdown from '.'
 
 describe('<CartDropdown />', () => {
-  it('should render the CartDropdown', () => {
-    renderWithTheme(<CartDropdown />)
+  it('should render <CartIcon /> and its badge', () => {
+    renderWithTheme(<CartDropdown items={items} total="R$ 300,00" />)
 
-    expect(
-      screen.getByRole('heading', { name: /CartDropdown/i })
-    ).toBeInTheDocument()
+    expect(screen.getByLabelText(/carrinho de comprar/i)).toBeInTheDocument()
+    expect(screen.getByText(`${items.length}`)).toBeInTheDocument()
+  })
+
+  it('should render Dropdown content with cart items and total', () => {
+    renderWithTheme(<CartDropdown items={items} total="R$ 300,00" />)
+
+    expect(screen.getByText('R$ 300,00')).toBeInTheDocument()
+    expect(screen.getByText(`${items[0].title}`)).toBeInTheDocument()
   })
 })
