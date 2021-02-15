ebdaccdfeee7bb5d41b2bda412c4047a5fea1aa3
Leandro Lopes <leandro-lopes46@hotmail.com>
2021-02-15 17:09:53 -0300

    Testing the UserDropdown component

--- src/components/UserDropdown/test.tsx
+++ src/components/UserDropdown/test.tsx
@@ -1,14 +1,24 @@
 import { screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 import { renderWithTheme } from 'utils/tests/helpers'
 
 import UserDropdown from '.'
 
 describe('<UserDropdown />', () => {
-  it('should render the UserDropdown', () => {
-    renderWithTheme(<UserDropdown />)
+  it('should render the username', () => {
+    renderWithTheme(<UserDropdown username="Leandro" />)
 
-    expect(
-      screen.getByRole('heading', { name: /UserDropdown/i })
-    ).toBeInTheDocument()
+    expect(screen.getByText(/leandro/i)).toBeInTheDocument()
+  })
+
+  it('should render the menu', () => {
+    renderWithTheme(<UserDropdown username="Leandro" />)
+
+    // open menu
+    userEvent.click(screen.getByText(/leandro/i))
+
+    expect(screen.getByRole('link', { name: /perfil/i })).toBeInTheDocument()
+    expect(screen.getByRole('link', { name: /favoritos/i })).toBeInTheDocument()
+    expect(screen.getByRole('link', { name: /sair/i })).toBeInTheDocument()
   })
 })
