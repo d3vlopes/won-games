0542ab07cc4bd06a50c584c6bc73a841ce376c4a
Leandro Lopes <leandro-lopes46@hotmail.com>
2021-02-15 17:07:50 -0300

    Creating structure of UserDropdown component

--- src/components/UserDropdown/index.tsx
+++ src/components/UserDropdown/index.tsx
@@ -1,9 +1,50 @@
+import Link from 'next/link'
+import {
+  AccountCircle,
+  FavoriteBorder,
+  ExitToApp
+} from '@styled-icons/material-outlined'
+import { ChevronDown } from '@styled-icons/boxicons-regular/ChevronDown'
+
+import Dropdown from 'components/Dropdown'
+
 import * as S from './styles'
 
-const UserDropdown = () => (
-  <S.Wrapper>
-    <h1>UserDropdown</h1>
-  </S.Wrapper>
+export type UserDropdownProps = {
+  username: string
+}
+
+const UserDropdown = ({ username }: UserDropdownProps) => (
+  <Dropdown
+    title={
+      <>
+        <AccountCircle size={24} />
+        <S.Username>{username}</S.Username>
+        <ChevronDown size={24} />
+      </>
+    }
+  >
+    <S.Nav>
+      <Link href="/profile/me" passHref>
+        <S.Link>
+          <AccountCircle />
+          <span>Perfil</span>
+        </S.Link>
+      </Link>
+      <Link href="/wishlist" passHref>
+        <S.Link>
+          <FavoriteBorder />
+          <span>Favoritos</span>
+        </S.Link>
+      </Link>
+      <Link href="/logout" passHref>
+        <S.Link title="Sair">
+          <ExitToApp />
+          <span>Sair</span>
+        </S.Link>
+      </Link>
+    </S.Nav>
+  </Dropdown>
 )
 
 export default UserDropdown
