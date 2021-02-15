6cf3fda9d6248d6d2bcf8b49dfead902fbe3d841
Leandro Lopes <leandro-lopes46@hotmail.com>
2021-02-15 17:08:50 -0300

    Config storybook of UserDropdown component

--- src/components/UserDropdown/stories.tsx
+++ src/components/UserDropdown/stories.tsx
@@ -1,9 +1,22 @@
 import { Story, Meta } from '@storybook/react/types-6-0'
-import UserDropdown from '.'
+import UserDropdown, { UserDropdownProps } from '.'
 
 export default {
   title: 'UserDropdown',
-  component: UserDropdown
+  component: UserDropdown,
+  parameters: {
+    backgrounds: {
+      default: 'won-dark'
+    }
+  }
 } as Meta
 
-export const Default: Story = () => <UserDropdown />
+export const Default: Story<UserDropdownProps> = (args) => (
+  <div style={{ maxWidth: '98%', display: 'flex', justifyContent: 'flex-end' }}>
+    <UserDropdown {...args} />
+  </div>
+)
+
+Default.args = {
+  username: 'Leandro'
+}
