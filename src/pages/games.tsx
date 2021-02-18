4047c5b6d6f80da14e78069d164e6b52984adbaf
Leandro Lopes <leandro-lopes46@hotmail.com>
2021-02-18 16:05:23 -0300

    Start populating games page

--- src/pages/games.tsx
+++ src/pages/games.tsx
@@ -1,16 +1,35 @@
 import GamesTemplate, { GamesTemplateProps } from 'templates/Games'
 
 import filterItemsMock from 'components/ExploreSidebar/mock'
-import gamesMock from 'components/GameCardSlider/mock'
+
+import { initializeApollo } from 'utils/apollo'
+import { QUERY_GAMES } from 'graphql/queries/games'
 
 export default function GamesPage(props: GamesTemplateProps) {
   return <GamesTemplate {...props} />
 }
 
-export function getServerSideProps() {
+export async function getStaticProps() {
+  const apolloClient = initializeApollo()
+
+  const { data } = await apolloClient.query({
+    query: QUERY_GAMES
+  })
+
   return {
     props: {
-      games: gamesMock,
+      // Recria a página dentro do tempo passado
+      revalidate: 60,
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      games: data.games.map((game: any) => ({
+        title: game.name,
+        developer: game.developers[0].name,
+        img: `http://localhost:1337${game.cover.url}`,
+        price: new Intl.NumberFormat('pt-BR', {
+          style: 'currency',
+          currency: 'BRL'
+        }).format(game.price)
+      })),
       filterItems: filterItemsMock
     }
   }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,23 +1,10 @@
-import { gql } from '@apollo/client'
-import { initializeApollo } from 'utils/apollo'
-
 import Home, { HomeTemplateProps } from 'templates/Home'
 
 import bannerMock from 'components/BannerSlider/mock'
 import gamesMock from 'components/GameCardSlider/mock'
 import highlightMock from 'components/Highlight/mock'
 
-const GET_GAMES = gql`
-  query getGames {
-    games {
-      name
-    }
-  }
-`
-
 export default function Index(props: HomeTemplateProps) {
-  if (props.data) return <p>{JSON.stringify(props.data, null, 2)}</p>
-
   return <Home {...props} />
 }
 
@@ -25,15 +12,9 @@ export default function Index(props: HomeTemplateProps) {
 // getServerSideProps => gera via ssr a cada request(não é possivel exportar os aquivos)
 // getInitialProps => Mesma coisa que o getServerSideProps, não tão utilizado ultimamente
 export async function getServerSideProps() {
-  const apolloClient = initializeApollo()
-
-  const { data } = await apolloClient.query({ query: GET_GAMES })
-
   // retorna os dados
   return {
     props: {
-      data: data,
-      initialApolloState: apolloClient.cache.extract(),
       banners: bannerMock,
       newGames: gamesMock,
       mostPopularHighlight: highlightMock,
