1f94ccd18b6a2f654c2b1bde1203638aa8b700a9
Leandro Lopes <leandro-lopes46@hotmail.com>
2021-02-18 17:14:49 -0300

    Add slug prop and link on GameCard component

--- src/components/GameCard/index.tsx
+++ src/components/GameCard/index.tsx
@@ -1,3 +1,5 @@
+import Link from 'next/link'
+
 import {
   FavoriteBorder,
   AddShoppingCart,
@@ -10,6 +12,7 @@ import Ribbon, { RibbonSize, RibbonColors } from 'components/Ribbon'
 import * as S from './styles'
 
 export type GameCardProps = {
+  slug: string
   title: string
   developer: string
   img: string
@@ -23,6 +26,7 @@ export type GameCardProps = {
 }
 
 const GameCard = ({
+  slug,
   title,
   developer,
   img,
@@ -41,15 +45,19 @@ const GameCard = ({
       </Ribbon>
     )}
 
-    <S.ImageBox>
-      <img src={img} alt={title} />
-    </S.ImageBox>
+    <Link href={`/game/${slug}`} passHref>
+      <S.ImageBox>
+        <img src={img} alt={title} />
+      </S.ImageBox>
+    </Link>
 
     <S.Content>
-      <S.Info>
-        <S.Title>{title}</S.Title>
-        <S.Developer>{developer}</S.Developer>
-      </S.Info>
+      <Link href={`/game/${slug}`} passHref>
+        <S.Info>
+          <S.Title>{title}</S.Title>
+          <S.Developer>{developer}</S.Developer>
+        </S.Info>
+      </Link>
 
       <S.FavButton onClick={onFav} role="button">
         {favorite ? (
