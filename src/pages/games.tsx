47a6b4e9012a48de6bfc0983cc9d220bc35e34f0
Leandro Lopes <leandro-lopes46@hotmail.com>
2021-03-01 15:45:01 -0300

    Removed price formatting from the games page

--- src/pages/games.tsx
+++ src/pages/games.tsx
@@ -27,10 +27,7 @@ export async function getStaticProps() {
         slug: game.slug,
         developer: game.developers[0].name,
         img: `http://localhost:1337${game.cover!.url}`,
-        price: new Intl.NumberFormat('pt-BR', {
-          style: 'currency',
-          currency: 'BRL'
-        }).format(game.price)
+        price: game.price
       })),
       filterItems: filterItemsMock
     }
