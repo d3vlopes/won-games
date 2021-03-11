cfdfc9fd81b3578ac534ae1adba1e41bb1c118f8
Leandro Lopes <leandro-lopes46@hotmail.com>
2021-03-11 16:27:43 -0300

    Config the test of Game template

--- src/templates/Games/test.tsx
+++ src/templates/Games/test.tsx
@@ -1,7 +1,11 @@
 import { screen } from '@testing-library/react'
+
+import { MockedProvider } from '@apollo/client/testing'
+
 import { renderWithTheme } from 'utils/tests/helpers'
 
-import gamesMock from 'components/GameCardSlider/mock'
+import { QUERY_GAMES } from 'graphql/queries/games'
+
 import filterItemsMock from 'components/ExploreSidebar/mock'
 
 import Games from '.'
@@ -20,24 +24,61 @@ jest.mock('components/ExploreSidebar', () => ({
   }
 }))
 
-jest.mock('components/GameCard', () => ({
-  __esModule: true,
-  default: function Mock() {
-    return <div data-testid="Mock GameCard" />
-  }
-}))
-
 describe('<Games />', () => {
-  it('should render sections', () => {
-    renderWithTheme(
-      <Games filterItems={filterItemsMock} games={[gamesMock[0]]} />
+  it('should render loading when starting the template', () => {
+    const { container } = renderWithTheme(
+      <MockedProvider mocks={[]} addTypename={false}>
+        <Games filterItems={filterItemsMock} />
+      </MockedProvider>
     )
 
-    expect(screen.getByTestId('Mock ExploreSidebar')).toBeInTheDocument()
-    expect(screen.getByTestId('Mock GameCard')).toBeInTheDocument()
+    expect(container.querySelector('#loading')).toBeInTheDocument()
+  })
+
+  it('should render sections', async () => {
+    const { container } = renderWithTheme(
+      <MockedProvider
+        mocks={[
+          {
+            request: {
+              query: QUERY_GAMES,
+              variables: { limit: 15 }
+            },
+            result: {
+              data: {
+                games: [
+                  {
+                    name: 'RimWorld',
+                    slug: 'rimworld',
+                    cover: {
+                      url: '/uploads/rimworld_8e93acc963.jpg'
+                    },
+                    developers: [{ name: 'Ludeon Studios' }],
+                    price: 65.99,
+                    __typename: 'Game'
+                  }
+                ]
+              }
+            }
+          }
+        ]}
+        addTypename={false}
+      >
+        <Games filterItems={filterItemsMock} />
+      </MockedProvider>
+    )
+
+    // No inicio mostra o loading
+    expect(container.querySelector('#loading')).toBeInTheDocument()
+
+    // Espera até encontrar o elemento
+    expect(await screen.findByTestId('Mock ExploreSidebar')).toBeInTheDocument()
+
+    // Procura pelo jogo
+    expect(await screen.findByText(/rimworld/i)).toBeInTheDocument()
 
     expect(
-      screen.getByRole('button', { name: /mostrar mais/i })
+      await screen.findByRole('button', { name: /mostrar mais/i })
     ).toBeInTheDocument()
   })
 })
