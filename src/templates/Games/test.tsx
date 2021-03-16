8c22030417314bd9de70a25c799261136393173c
Leandro Lopes <leandro-lopes46@hotmail.com>
2021-03-16 16:02:17 -0300

    Testing the ExploreSidebar component inside Games template

--- src/templates/Games/test.tsx
+++ src/templates/Games/test.tsx
@@ -10,17 +10,21 @@ import filterItemsMock from 'components/ExploreSidebar/mock'
 import Games from '.'
 import { fetchMoreMock, gamesMock } from './mocks'
 
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const useRouter = jest.spyOn(require('next/router'), 'useRouter')
+const push = jest.fn()
+
+useRouter.mockImplementation(() => ({
+  push,
+  query: '',
+  asPath: '',
+  route: '/'
+}))
+
 jest.mock('templates/Base', () => ({
   __esModule: true,
   default: function Mock({ children }: { children: React.ReactNode }) {
     return <div data-testid="Mock Base">{children}</div>
-  }
-}))
-
-jest.mock('components/ExploreSidebar', () => ({
-  __esModule: true,
-  default: function Mock({ children }: { children: React.ReactNode }) {
-    return <div data-testid="Mock ExploreSidebar">{children}</div>
   }
 }))
 
@@ -46,7 +50,7 @@ describe('<Games />', () => {
     expect(container.querySelector('#loading')).toBeInTheDocument()
 
     // Espera até encontrar o elemento
-    expect(await screen.findByTestId('Mock ExploreSidebar')).toBeInTheDocument()
+    expect(await screen.findByText(/preço/i)).toBeInTheDocument()
 
     // Procura pelo jogo
     expect(await screen.findByText(/sample game/i)).toBeInTheDocument()
@@ -70,5 +74,23 @@ describe('<Games />', () => {
     )
 
     expect(await screen.findByText(/fetch more game/i)).toBeInTheDocument()
+  })
+
+  // Testa se os filtros estão sendo executas corretamente
+  it('should change push router when selecting a filter', async () => {
+    renderWithTheme(
+      <MockedProvider mocks={[gamesMock, fetchMoreMock]} cache={apolloCache}>
+        <Games filterItems={filterItemsMock} />
+      </MockedProvider>
+    )
+
+    userEvent.click(await screen.findByRole('checkbox', { name: /windows/i }))
+    userEvent.click(await screen.findByRole('checkbox', { name: /linux/i }))
+    userEvent.click(await screen.findByLabelText(/do menor para o maior/i))
+
+    expect(push).toHaveBeenCalledWith({
+      pathname: '/games',
+      query: { platforms: ['windows', 'linux'], sort_by: 'low-to-high' }
+    })
   })
 })
