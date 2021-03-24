317738e6a48693adbbcbf3eeedef3fb4233a6a6f
Leandro Lopes <leandro-lopes46@hotmail.com>
2021-03-24 16:36:56 -0300

    Add CartProvider to _app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,6 +4,7 @@ import { AppProps } from 'next/app'
 import Head from 'next/head'
 
 import { useApollo } from 'utils/apollo'
+import { CartProvider } from 'hooks/use-cart'
 
 import GlobalStyles from 'styles/global'
 import theme from 'styles/theme'
@@ -14,15 +15,20 @@ function App({ Component, pageProps }: AppProps) {
   return (
     <ApolloProvider client={client}>
       <ThemeProvider theme={theme}>
-        <Head>
-          <title>Won Games</title>
-          <link rel="shortcut icon" href="/img/icon-512.png" />
-          <link rel="apple-touch-icon" href="/img/icon-512.png" />
-          <link rel="manifest" href="/manifest.json" />
-          <meta name="description" content="A melhor loja de games do mundo!" />
-        </Head>
-        <GlobalStyles />
-        <Component {...pageProps} />
+        <CartProvider>
+          <Head>
+            <title>Won Games</title>
+            <link rel="shortcut icon" href="/img/icon-512.png" />
+            <link rel="apple-touch-icon" href="/img/icon-512.png" />
+            <link rel="manifest" href="/manifest.json" />
+            <meta
+              name="description"
+              content="A melhor loja de games do mundo!"
+            />
+          </Head>
+          <GlobalStyles />
+          <Component {...pageProps} />
+        </CartProvider>
       </ThemeProvider>
     </ApolloProvider>
   )
