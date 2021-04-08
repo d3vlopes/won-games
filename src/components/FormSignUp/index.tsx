2841bbea1854431fb40ff7ed79f5fdb2a1ad1299
Leandro Lopes <leandro-lopes46@hotmail.com>
2021-04-08 19:35:08 -0300

    Add validations to FormSignUp component

--- src/components/FormSignUp/index.tsx
+++ src/components/FormSignUp/index.tsx
@@ -1,18 +1,27 @@
-import Link from 'next/link'
-import { useState } from 'react'
+import React, { useState } from 'react'
 import { useMutation } from '@apollo/client'
+
+import Link from 'next/link'
 import { signIn } from 'next-auth/client'
-import { AccountCircle, Email, Lock } from '@styled-icons/material-outlined'
+import {
+  AccountCircle,
+  Email,
+  ErrorOutline,
+  Lock
+} from '@styled-icons/material-outlined'
 
 import { UsersPermissionsRegisterInput } from 'graphql/generate/globalTypes'
 import { MUTATION_REGISTER } from 'graphql/mutations/register'
 
+import { FieldErrors, signUpValidate } from 'utils/validations'
+
+import { FormWrapper, FormLink, FormLoading, FormError } from 'components/Form'
 import Button from 'components/Button'
 import TextField from 'components/TextField'
-import Loading from 'components/Loading'
-import { FormWrapper, FormLink } from 'components/Form'
 
 const FormSignUp = () => {
+  const [formError, setFormError] = useState('')
+  const [fieldError, setFieldError] = useState<FieldErrors>({})
   const [values, setValues] = useState<UsersPermissionsRegisterInput>({
     username: '',
     email: '',
@@ -20,7 +29,11 @@ const FormSignUp = () => {
   })
 
   const [createUser, { error, loading }] = useMutation(MUTATION_REGISTER, {
-    onError: (err) => console.error(err),
+    onError: (err) =>
+      setFormError(
+        err?.graphQLErrors[0]?.extensions?.exception.data.message[0].messages[0]
+          .message
+      ),
     onCompleted: () => {
       !error &&
         signIn('credentials', {
@@ -31,8 +44,22 @@ const FormSignUp = () => {
     }
   })
 
+  const handleInput = (field: string, value: string) => {
+    setValues((s) => ({ ...s, [field]: value }))
+  }
+
   const handleSubmit = async (event: React.FormEvent) => {
     event.preventDefault()
+    setFormError('')
+
+    const errors = signUpValidate(values)
+
+    if (Object.keys(errors).length) {
+      setFieldError(errors)
+      return
+    }
+
+    setFieldError({})
 
     createUser({
       variables: {
@@ -45,51 +72,49 @@ const FormSignUp = () => {
     })
   }
 
-  const handleInput = (field: string, value: string) => {
-    setValues((s) => ({ ...s, [field]: value }))
-  }
-
   return (
     <FormWrapper>
+      {!!formError && (
+        <FormError>
+          <ErrorOutline /> {formError}
+        </FormError>
+      )}
       <form onSubmit={handleSubmit}>
         <TextField
           name="username"
           placeholder="Nome de usuário"
           type="text"
-          onInputChange={(value) => handleInput('username', value)}
+          error={fieldError?.username}
+          onInputChange={(v) => handleInput('username', v)}
           icon={<AccountCircle />}
         />
-
         <TextField
           name="email"
           placeholder="Email"
-          type="email"
-          onInputChange={(value) => handleInput('email', value)}
+          type="text"
+          error={fieldError?.email}
+          onInputChange={(v) => handleInput('email', v)}
           icon={<Email />}
         />
-
         <TextField
           name="password"
           placeholder="Senha"
           type="password"
-          onInputChange={(value) => handleInput('password', value)}
+          error={fieldError?.password}
+          onInputChange={(v) => handleInput('password', v)}
           icon={<Lock />}
         />
-
         <TextField
-          name="confirm-password"
+          name="confirm_password"
           placeholder="Repita a senha"
           type="password"
-          onInputChange={(value) => handleInput('confirm-password', value)}
+          error={fieldError?.confirm_password}
+          onInputChange={(v) => handleInput('confirm_password', v)}
           icon={<Lock />}
         />
 
         <Button type="submit" size="large" fullWidth disabled={loading}>
-          {loading ? (
-            <Loading color="white" size={4.5} />
-          ) : (
-            <span>Criar Conta</span>
-          )}
+          {loading ? <FormLoading /> : <span>Criar conta</span>}
         </Button>
 
         <FormLink>
