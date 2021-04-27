601fc41d01b97f199a0632cb1ef20a6b272ad691
Leandro Lopes <leandro-lopes46@hotmail.com>
2021-04-27 19:32:02 -0300

    Add isInWishlist method on use-wishlist

--- src/hooks/use-wishlist/index.tsx
+++ src/hooks/use-wishlist/index.tsx
@@ -1,5 +1,6 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
 /* eslint-disable @typescript-eslint/no-empty-function */
+/* eslint-disable @typescript-eslint/no-unused-vars */
+
 import { useSession } from 'next-auth/client'
 import { createContext, useContext, useEffect, useState } from 'react'
 
@@ -40,10 +41,6 @@ const WishlistProvider = ({ children }: WishlistProviderProps) => {
     QueryWishlist_wishlists_games[]
   >([])
 
-  const isInWishlist = (id: string) => false
-  const addToWishlist = (id: string) => {}
-  const removeFromWishlist = (id: string) => {}
-
   const { data, loading } = useQueryWishlist({
     skip: !session?.user?.email,
     context: { session },
@@ -55,6 +52,12 @@ const WishlistProvider = ({ children }: WishlistProviderProps) => {
   useEffect(() => {
     setWishlistItems(data?.wishlists[0]?.games || [])
   }, [data])
+
+  const isInWishlist = (id: string) =>
+    !!wishlistItems.find((game) => game.id === id)
+
+  const addToWishlist = (id: string) => {}
+  const removeFromWishlist = (id: string) => {}
 
   return (
     <WishlistContext.Provider
