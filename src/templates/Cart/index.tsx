ee7075c4e8fa3f43c0745150e782e2fcb631c2bd
Leandro Lopes <leandro-lopes46@hotmail.com>
2021-05-04 00:48:34 -0300

    Add warning text on Cart template

--- src/templates/Cart/index.tsx
+++ src/templates/Cart/index.tsx
@@ -1,6 +1,8 @@
 import { loadStripe } from '@stripe/stripe-js'
 import { Elements } from '@stripe/react-stripe-js'
 
+import { Info } from '@styled-icons/material-outlined/Info'
+
 import Base from 'templates/Base'
 
 import { Container } from 'components/Container'
@@ -41,6 +43,16 @@ const Cart = ({
             <PaymentForm />
           </Elements>
         </S.Content>
+
+        <S.Text>
+          <Info size={18} /> {''}
+          Sua compra é protegida por uma conexão segura da plataforma WON. Ao
+          comprar em nossa loja, você concorda com nossos
+          <a href="#"> termos de uso.</a> Após efetuar a compra você tem direito
+          ao reembolso no prazo máximo de 30 dias, sem nenhum custo adicional,
+          desde que o download do jogo adquirido não tenha ocorrido após a sua
+          compra.
+        </S.Text>
 
         <Divider />
       </Container>
