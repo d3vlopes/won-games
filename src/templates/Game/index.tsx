298487a42e1ebed87df8e640f975e7985d62cfa9
Leandro Lopes <leandro-lopes46@hotmail.com>
2021-05-19 19:30:16 -0300

    Add NextSeo to Game Page

--- src/templates/Game/index.tsx
+++ src/templates/Game/index.tsx
@@ -1,3 +1,5 @@
+import { NextSeo } from 'next-seo'
+
 import Base from 'templates/Base'
 
 import * as S from './styles'
@@ -12,6 +14,7 @@ import TextContent from 'components/TextContent'
 import Showcase from 'components/Showcase'
 
 export type GameTemplateProps = {
+  slug?: string
   cover: string
   gameInfo: GameInfoProps
   gallery?: GalleryImageProps[]
@@ -25,6 +28,7 @@ export type GameTemplateProps = {
 }
 
 const Game = ({
+  slug,
   cover,
   gameInfo,
   gallery,
@@ -37,6 +41,22 @@ const Game = ({
   recommendedGames
 }: GameTemplateProps) => (
   <Base>
+    <NextSeo
+      title={`${gameInfo.title} - Won Games`}
+      description={gameInfo.description}
+      canonical={`https://wongames.com.br/game/${slug}`}
+      openGraph={{
+        url: `https://wongames.com.br/game/${slug}`,
+        title: `${gameInfo.title} - Won Games`,
+        description: gameInfo.description,
+        images: [
+          {
+            url: cover,
+            alt: `${gameInfo.title}`
+          }
+        ]
+      }}
+    />
     <S.Cover src={cover} role="image" aria-label="cover" />
     <S.Main>
       <S.SectionGameInfo>
